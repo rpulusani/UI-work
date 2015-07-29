25590ecce8160faba9b48c85b4b04c3cd58b8975
bradleywb <brabarton@lexmark.com>
2015-07-29 14:04:33 -0400

    attempt numero uno

--- src/client/js/routes.js
+++ src/client/js/routes.js
@@ -2,6 +2,14 @@ angular.module('mps')
   .config(['$routeProvider',
     function($routeProvider) {
     $routeProvider
+    .when('/service_requests/addresses', {
+        templateUrl: '/js/service_requests/addresses/view.html',
+        controller: 'AddressesController'
+    })
+    .when('/service_requests/addresses/delete', {
+        templateUrl: '/js/service_requests/addresses/delete.html',
+        controller: 'AddressesController'
+    })
     .when('/service_requests/addresses/new', {
         templateUrl: '/js/service_requests/addresses/new.html',
         controller: 'AddressesController'
--- src/client/js/service_requests/addresses/addresses-controller.js
+++ src/client/js/service_requests/addresses/addresses-controller.js
@@ -1,5 +1,5 @@
  angular.module('mps')
-    .controller("AddressesController", ['$scope', function($scope){
+    .controller("AddressesController", ['$scope', '$location', 'Addresses', function($scope, $location, Addresses){
 
     $scope.continueForm = false;
 
@@ -52,15 +52,14 @@
         if($scope.continueForm){
             $scope.continueForm = false;
         }else{
-            window.location="/";
+            $location.path("/");
         }
 
         return false;
     };
 
     $scope.cancel = function(){
-        console.log("cancel");
-        window.location="/";
+        $location.path("/");
         return false;
     };
 
@@ -68,6 +67,39 @@
 
     $scope.attachmentToggle = function(){
         $scope.attachmentIsShown = !$scope.attachmentIsShown;
+    };
+
+    $scope.addresses = Addresses.query();
+
+    $scope.selectedAddress = null;
+
+    $scope.getAddress = function(id){
+        var length = $scope.addressses.length,
+            addresses = $scope.addresses,
+            address = {};
+        for(var i = 0; i < length; ++i) {
+            if(addresses[i].id === id) {
+                address = addresses[i];
+                break;
+            }
+        }
+        return address;
+    };
+
+    $scope.cancelDelete = function(){
+        window.location = "#/service_requests/addresses";
+        return false;
+    };
+
+    $scope.requestDelete = function(){
+        var addressId = $location.search().id;
+        console.log("Requested deletion of " + $scope.getAddress(addressId).addName);
+        return false;
+    };
+
+    $scope.deleteAddress = function(id) {
+        $scope.selectedAddress = $scope.getAddress(id);
+        $location.path("#/service_requests/addresses/delete");
     };
 
 }])
@@ -82,7 +114,30 @@ function() {
             city: 'Lexington',
             country: 'USA',
             state: 'KY',
-            zipCode: '40404'
+            zipCode: '40404',
+            id: "foo/1"
+        },
+        {
+            addName: 'Addy 2',
+            storeName: 'Another Store',
+            addrLine1: '123 Some Other Rd',
+            addrLine2: null,
+            city: 'Lexington',
+            country: 'USA',
+            state: 'KY',
+            zipCode: '40404',
+            id: "foo/2"
+        },
+        {
+            addName: 'Addy 3',
+            storeName: 'Yet Another Store',
+            addrLine1: '123 Long Cat Is Really Loooooooooooooong Rd',
+            addrLine2: null,
+            city: 'Lexington',
+            country: 'USA',
+            state: 'KY',
+            zipCode: '40404',
+            id: "foo/3"
         }
     ];
     return {
