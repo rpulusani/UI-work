4ab6f1f9eba7100b1d410555862bee2e9d8ed705
Rocky Bevins <moreoutput@gmail.com>
2015-08-06 10:41:12 -0400

    cleaning modular pattern

--- src/client/js/address_service_request/directives/directive.js
+++ src/client/js/address_service_request/directives/directive.js
@@ -14,19 +14,19 @@ angular.module('mps.serviceRequestAddresses')
 .directive('primaryRequestContact', function() {
     return {
         restrict: 'E',
-        templateUrl: '/js/service_requests/common/primary-request-contact.html'
+        templateUrl: '/js/service_requests/templates/primary-request-contact.html'
     };
 })
 .directive('additionalRequestInfo', function() {
     return {
         restrict: 'E',
-        templateUrl: '/js/service_requests/common/additional-request-info.html'
+        templateUrl: '/js/service_requests/templates/additional-request-info.html'
     };
 })
 .directive('addressUpload', function() {
     return {
         restrict: 'E',
-        templateUrl: '/js/service_requests/common/address-upload.html'
+        templateUrl: '/js/service_requests/templates/address-upload.html'
     };
 })
 .directive('readAddress', function() {
--- src/client/js/service_requests/serviceRequests.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-angular.module('mps.serviceRequests', []).controller('ServiceRequestController',
-    ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams) {
-}]);
