2068551038d9c3910b740b54dd53b56278cc79a7
Creighton Medley <creighton.medley@gmail.com>
2015-08-17 11:06:12 -0400

    updating directives and html for ie8

--- src/client/app/service_requests/directives.js
+++ src/client/app/service_requests/directives.js
@@ -2,19 +2,19 @@
 angular.module('mps.serviceRequests')
 .directive('primaryRequestContact', function() {
     return {
-        restrict: 'E',
+        restrict: 'A',
         templateUrl: '/app/service_requests/templates/primary-request-contact.html'
     };
 })
 .directive('additionalRequestInfo', function() {
     return {
-        restrict: 'E',
+        restrict: 'A',
         templateUrl: '/app/service_requests/templates/additional-request-info.html'
     };
 })
 .directive('addressUpload', function() {
     return {
-        restrict: 'E',
+        restrict: 'A',
         templateUrl: '/app/service_requests/templates/address-upload.html',
         controller: ['$scope', function($scope) {
             $scope.file_list = ['.csv', '.xls', '.xlsx', '.vsd', '.doc',
