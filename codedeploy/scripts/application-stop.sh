3d0f90b2cf1f8bf6a0d543c91c26cae6ec87bd13
Jeff Stice-Hall <jeff.hall@digitalmaelstrom.net>
2015-08-17 21:13:02 -0500

    Remove named container during CodeDeploy stop event

--- codedeploy/scripts/application-stop.sh
+++ codedeploy/scripts/application-stop.sh
@@ -5,5 +5,6 @@ containerName="mps-ui"
 isRunning=`docker ps --filter "name=$containerName" | tail -n 1 | awk '{print $1}'`
 if [[ $isRunning -ne "CONTAINER" ]]; then
   docker stop $containerName
+  docker rm $containerName
 fi
 
