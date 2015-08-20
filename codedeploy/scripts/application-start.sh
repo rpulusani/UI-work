338da31aa9b9de7f01341d0c32b869cc2aafb61d
Kristopher Stice <kris.stice@lexmark.com>
2015-08-20 16:23:06 -0500

    Update application-start.sh

--- codedeploy/scripts/application-start.sh
+++ codedeploy/scripts/application-start.sh
@@ -1,9 +1,9 @@
 docker pull lexmarkweb/mps-ui
 if [ "$DEPLOYMENT_GROUP_NAME" == "mps-ui-dev" ]
 then
-environmentVars="-e 'NEWRELICID=10059346'"
+environmentVars="-e ENVIRONMENT=INTEGRATED -e NEWRELICID=10059346"
 else
-environmentVars="-e 'NEWRELICID=10059346'"
+environmentVars="-e ENVIRONMENT=UNKNOWN -e NEWRELICID=10059346 -e NEW_RELIC_LICENSE_KEY=0ce62264499457b6bb48fc18ed2ee650deeb6e2e -e NEW_RELIC_APP_NAME=MPS-DEV -e NEW_RELIC_BROWSER_MONITOR_ENABLE=true -e NEW_RELIC_CAPTURE_PARAMS=true -e NEW_RELIC_LOG_LEVEL=info -e NEW_RELIC_ENABLED=true -e NEW_RELIC_TRACER_ENABLED=true"
 fi
 
 docker run -i -d -p=8080:8080 --restart=always $environmentVars --name mps-ui lexmarkweb/mps-ui
