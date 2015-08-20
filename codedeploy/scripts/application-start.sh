b16b46890ea09516f3f1566b9e1cd29ddc097391
Kristopher Stice <kris.stice@lexmark.com>
2015-08-20 16:15:21 -0500

    Update application-start.sh

--- codedeploy/scripts/application-start.sh
+++ codedeploy/scripts/application-start.sh
@@ -1,10 +1,9 @@
 docker pull lexmarkweb/mps-ui
-printenv
 if [ "$DEPLOYMENT_GROUP_NAME" == "mps-ui-dev" ]
 then
 environmentVars="-e 'NEWRELICID=10059346'"
 else
-environmentVars=""
+environmentVars="-e 'NEWRELICID=10059346'"
 fi
 
 docker run -i -d -p=8080:8080 --restart=always $environmentVars --name mps-ui lexmarkweb/mps-ui
