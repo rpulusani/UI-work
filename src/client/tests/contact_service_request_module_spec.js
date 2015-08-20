a3a752dab2d4032c415eb91cfc590b3e087a2111
Rocky Bevins <rockybevins@gmail.com>
2015-08-20 10:13:34 -0400

    Tests for contact service requests

--- src/client/tests/contact_service_request_module_spec.js
+++ src/client/tests/contact_service_request_module_spec.js
@@ -1,7 +1,8 @@
 /* global describe it beforeEach inject expect */
 
-describe('Address Service Request Module', function() {
+describe('Contact Service Request Module', function() {
     beforeEach(module('mps'));
+
     describe('Controllers', function(){
         var scope, ctrl, location, contacts, window, history;
 
@@ -10,20 +11,60 @@ describe('Address Service Request Module', function() {
             location = $location;
             window = $window;
             history = History;
-            ctrl = $controller('contactsController', {$scope: scope});
+            ctrl = $controller('ContactsController', {$scope: scope});
             contacts = Contacts;
         }));
+
+         describe('when backed up', function() {
+            describe('when continueForm is true', function() {
+                it('should set continueForm to be false', function() {
+                    spyOn(history,'back').and.returnValue('/service_requests/contacts');
+                    scope.continueForm = true;
+                    scope.back();
+                    expect(scope.continueForm).toBe(false);
+                });
+            });
+
+            describe('when continueForm is false', function() {
+                it('should return to home', function() {
+                    spyOn(history,'back').and.returnValue('/service_requests/contacts');
+                    scope.continueForm = false;
+                    scope.back();
+                    expect(scope.continueForm).toEqual(false);
+                });
+            });
+
+            describe('when cancelled', function() {
+            it('should return to home', function(){
+                spyOn(location, 'path').and.returnValue('/');
+                scope.cancel();
+                expect(location.path).toHaveBeenCalledWith('/service_requests/contacts');
+                });
+            });
+
+            describe('when continued', function() {
+              it('should set continueForm to be true', function() {
+                  scope.continue();
+                  expect(scope.continueForm).toBe(true);
+              });
+            });
+
+            describe('when a delete request is cancelled', function() {
+                it('should return to the all contacts view', function() {
+                    spyOn(location, 'path').and.returnValue('/service_requests/contacts');
+                    scope.cancel();
+                    expect(location.path).toHaveBeenCalledWith('/service_requests/contacts');
+                });
+            });
+        });
     });
 
-    describe("Routes", function(){
+    describe('Routes', function(){
         it('should map routes to controllers', function() {
             inject(function($route) {
-                /*  only test logic examples not anything more
-                expect($route.routes['/service_requests/contacts'].controller).toBe('contactsController');
-                expect($route.routes['/service_requests/contacts'].templateUrl).toEqual('/js/address_service_requests/templates/view.html');
-                expect($route.routes['/service_requests/contacts/delete'].templateUrl).toEqual('/js/address_service_requests/templates/delete.html');
-                expect($route.routes['/service_requests/contacts/new'].templateUrl).toEqual('/js/address_service_requests/templates/new.html');
-                */
+                expect($route.routes['/service_requests/contacts'].controller).toBe('ContactsController');
+                expect($route.routes['/service_requests/contacts'].templateUrl).toEqual('/app/contact_service_requests/templates/view.html');
+                expect($route.routes['/service_requests/contacts/new'].templateUrl).toEqual('/app/contact_service_requests/templates/new.html');
             });
         });
 
@@ -36,7 +77,7 @@ describe('Address Service Request Module', function() {
             beforeEach(inject(function($rootScope, $compile) {
                 scope = $rootScope.$new();
 
-                element = '<address-new-fields></address-new-fields>';
+                element = '<contact-new-fields></contact-new-fields>';
                 element = $compile(element)(scope);
 
                 scope.$digest();
