53037833319e2b6a0d4a00adcbf0c0f8f24fe6be
Kristopher Stice <kris.stice@lexmark.com>
2015-08-20 15:50:45 -0500

    Update application-start.sh
    Adding in environmental variables

--- codedeploy/scripts/application-start.sh
+++ codedeploy/scripts/application-start.sh
@@ -1,2 +1,10 @@
 docker pull lexmarkweb/mps-ui
-docker run -i -d -p=8080:8080 --restart=always --name mps-ui lexmarkweb/mps-ui
+
+if [ "$DEPLOYMENT_GROUP_NAME" == "mps-ui-dev" ]
+then
+environmentVars="-e 'NEWRELICID=10059346'"
+else
+environmentVars=""
+fi
+
+docker run -i -d -p=8080:8080 --restart=always $environmentVars --name mps-ui lexmarkweb/mps-ui
