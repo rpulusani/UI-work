da9a147faad9eb9cbf11b67c7f8d3fa4a8d63690
Kris Stice <kris.stice@perceptivesoftware.com>
2015-08-31 15:54:22 -0500

    Turn Off Selectric

--- src/client/app/form/directives.js
+++ src/client/app/form/directives.js
@@ -3,11 +3,6 @@ angular.module('mps.form')
     function(){
         return {
             restrict: 'E',
-angular.module('mps.form')
-.directive('input',[
-    function(){
-        return {
-            restrict: 'E',
             require: ['?ngModel'],
             link: function(scope, el, attr, model){
                 if (!model || model.length < 1) {
@@ -26,34 +21,33 @@ angular.module('mps.form')
         };
     }
 ])
-.directive('select', [
-        function() {
+.directive('select', ['$timeout',
+        function($timeout) {
             return {
                 restrict: 'E',
                 require: ['?ngModel'],
                 link: function(scope, el, attr, model) {
-                      /*  if (!model || model.length < 2) {
-                            return;
-                        }*/
                         var $ = require('jquery');
-                        if(el.length > 0){
+
+                        /*if(el.length > 0){
                             var options = el[0]["options"],
                             length = el[0]["options"].length;
                            for(var i = 0; i < length; ++i){
                             if(options[i]["defaultSelected"]){
                                 var value =  options[i]["value"];
                                 console("Value: " + value);
-                                 model[1].$setViewValue(value);
+                                 model[0].$setViewValue(value);
                             }
                            }
-                        }
-                        $(el).selectric({
+                        }*/
+
+                      /*  $(el).selectric({
                             onChange: function(){
-                               var value = $(el).val();
+                                var value = $(el).val();
                                 model[0].$setViewValue(value);
                             }
                         });
-                        /*scope.$watch(function() {
+                        scope.$watch(function() {
                             return el[0].length;
                         }, function() {
                             $(el).selectric('refresh');
