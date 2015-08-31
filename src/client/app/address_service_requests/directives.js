bff3ced0e1e2599eb4980fd55400b1fef662a0db
Kris Stice <kris.stice@perceptivesoftware.com>
2015-08-30 22:55:55 -0500

    [WIP] - Page workflow for new address.

--- src/client/app/address_service_requests/directives.js
+++ src/client/app/address_service_requests/directives.js
@@ -26,19 +26,4 @@ angular.module('mps.serviceRequestAddresses')
         restrict: 'A',
         templateUrl: '/app/address_service_requests/templates/read.html'
     };
-})
-.directive('fileModel', ['$parse', function ($parse) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            var model = $parse(attrs.fileModel),
-            modelSetter = model.assign;
-
-            element.bind('change', function() {
-                scope.$apply(function(){
-                    modelSetter(scope, element[0].files[0]);
-                });
-            });
-        }
-    };
-}]);
+});
