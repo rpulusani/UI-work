9b45f624fdab8f697ce85ab3be40279b1ea077d0
Ayu Demura <ayu.demura@perceptivesoftware.com>
2015-08-31 10:34:40 -0400

    Add country service api plugin.

--- src/client/app/address_service_requests/directives.js
+++ src/client/app/address_service_requests/directives.js
@@ -3,10 +3,10 @@ angular.module('mps.serviceRequestAddresses')
 .directive('addressNewFields', function() {
     return {
         restrict: 'A',
-        scope: {},
         templateUrl: '/app/address_service_requests/templates/address-new-fields.html',
         controller: ['$scope', 'CountryService', function($scope, CountryService){
             $scope.countryHAL = CountryService.getHAL();
+        }]
         }]
     };
 })
