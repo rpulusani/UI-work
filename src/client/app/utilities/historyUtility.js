49c5586f42182612fc2e17da4aa5f042b067e4a8
Ayu Demura <ayu.demura@perceptivesoftware.com>
2015-08-31 11:20:29 -0400

    move halInterceptor and CountryService into more appropriate location.

--- src/client/app/utilities/historyUtility.js
+++ src/client/app/utilities/historyUtility.js
@@ -10,22 +10,4 @@ angular.module('mps.utility')
     };
 
     return new History();
-}])
-
-.factory('halInterceptor', function() {
-    return {
-        response: function(response) {
-            angular.copy(response.data._embedded, response.resource);
-            return response;
-        }
-    }
-})
-
-.factory('CountryService', ['$resource', 'mpsApiUri', 'halInterceptor',
-    function($resource, mpsApiUri, halInterceptor) {
-        var url = mpsApiUri + '/countries';
-        return $resource(url, {}, {
-            'getHAL': { method: 'GET', url: url, interceptor: halInterceptor }
-        });
-    }
-]);
+}]);
--- src/client/app/utilities/utility.js
+++ src/client/app/utilities/utility.js
@@ -1,2 +1,10 @@
 'use strict';
-angular.module('mps.utility', []);
+angular.module('mps.utility', [])
+.factory('CountryService', ['$resource', 'mpsApiUri', 'halInterceptor',
+    function($resource, mpsApiUri, halInterceptor) {
+        var url = mpsApiUri + '/countries';
+        return $resource(url, {}, {
+            'getHAL': { method: 'GET', url: url, interceptor: halInterceptor }
+        });
+    }
+]);
