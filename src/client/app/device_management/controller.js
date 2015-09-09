5772f32e24edc912262f998394305ce429aff11e
sumandg86 <sumand@lexmark.com>
2015-09-09 13:14:52 -0400

    [Us99204] - Initial Commit for Device Information page.

---
+++ src/client/app/device_management/controller.js
@@ -0,0 +1,7 @@
+'use strict';
+angular.module('mps.deviceManagement')
+.controller('DeviceManagementController', ['$scope', '$location', '$routeParams', 'History',
+    function($scope, $location, $routeParams, History) {
+
+    }
+]);
---
+++ src/client/app/device_management/directives.js
@@ -0,0 +1,9 @@
+'use strict';
+angular.module('mps.deviceManagement')
+.directive('deviceInformation', function() {
+    return {
+        restrict: 'A',
+        templateUrl: '/app/device_management/templates/device-information.html',
+        controller: 'DeviceManagementController' 
+    };
+});
