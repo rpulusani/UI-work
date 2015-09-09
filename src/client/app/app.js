33b1bd9cab049ad905052c8431ec6cd90b218db8
Kris Stice <kris.stice@perceptivesoftware.com>
2015-09-08 19:00:46 -0500

    New Address and Unit Tests

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -45,7 +45,6 @@ angular.module('mps', [
 .constant('serviceUrl', config.portal.serviceUrl)
 
 .config(function(GatekeeperProvider, serviceUrl){
-  console.log("the serviceUrl: " + serviceUrl);
   GatekeeperProvider.configure({
     serviceUri: config.idp.serviceUrl,
     clientId: config.idp.clientId
