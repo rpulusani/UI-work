533e8e0a59493f076b9a346f5fe7594c46812c6f
Ayu Demura <ayu.demura@perceptivesoftware.com>
2015-09-11 15:05:44 -0400

    Use serviceUrl instead of mpsApiUri.

--- src/client/app/device_management/deviceManagementFactory.js
+++ src/client/app/device_management/deviceManagementFactory.js
@@ -1,12 +1,10 @@
 define(['angular', 'deviceManagement'], function(angular) {
     'use strict';
     angular.module('mps.deviceManagement')
-    .factory('Device', ['$resource',
-        function($resource) {
-            var url = '/accounts/:accountId/devices/:id';
-            return $resource(url, {accountId: '@accountId', id: '@id'}, {
-
-            });
+    .factory('Device', 'serviceUrl', ['$resource', 'serviceUrl',
+        function($resource, serviceUrl) {
+            var url =  serviceUrl + '/accounts/:accountId/devices/:id';
+            return $resource(url, {accountId: '@accountId', id: '@id'}, {});
         }
     ]);
 });
