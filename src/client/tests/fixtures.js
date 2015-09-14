9614876fe8807f3ec433c7d996abe8c5b8a83206
Ayu Demura <ayu.demura@perceptivesoftware.com>
2015-09-14 11:41:39 -0400

    Fixed new test breakage due to account.

--- src/client/tests/fixtures.js
+++ src/client/tests/fixtures.js
@@ -6,7 +6,14 @@ define([], function() {
             users: [
                 {
                     id: 1,
-                    idpId: "1"
+                    idpId: "1",
+                    _links: {
+                        accounts: [
+                            {
+                                href: "http://10.145.116.233:8080/mps/accounts/1"
+                            }
+                        ]
+                    }
                 }
             ]
         }
