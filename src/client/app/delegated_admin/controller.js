dc2dfd3ba17f45e8d4112a6c5b07403ea07bb624
Ayu Demura <ayu.demura@perceptivesoftware.com>
2015-09-21 15:15:39 -0400

    Add UX invited user list.

--- src/client/app/delegated_admin/controller.js
+++ src/client/app/delegated_admin/controller.js
@@ -4,11 +4,14 @@ define(['angular', 'delegatedAdmin'], function(angular) {
     .controller('DelegatedAdminController', ['$scope', '$location', '$routeParams', '$rootScope',
         function($scope, $location, $routeParams, $rootScope) {
 
+            $scope.all_users_active = true;
+            $scope.invitations_active = false;
+
             // TODO: remove hardcode when api is ready.
-            // UserService.query(function(users) {
-            //     $scope.users = users;
+            // UserService.query({activeStatus: 'Y'}, function(users) {
+            //     $scope.all_users = users;
             // });
-            $scope.users = [
+            $scope.all_users = [
                 {status: 'Active', created: '09/01/15', id: 1234567890,
                  lastName: 'Public', firstName: 'John', email: 'jpublic@lexmark.com',
                  account: {name: 'Lexmark International, Inc'}, roles: 'End user'},
@@ -29,7 +32,50 @@ define(['angular', 'delegatedAdmin'], function(angular) {
                  account: {name: 'Lexmark International, Inc'}, roles: 'End user'}
             ];
 
+            // TODO: remove hardcode when api is ready.
+            // UserService.query({activeStatus: 'N'}, function(users) {
+            //     $scope.invited_users = users;
+            // });
+            $scope.invited_users = [
+                {status: 'Pending', created: '09/01/15', id: 1234567890,
+                 lastName: 'Public', firstName: 'John', email: 'jpublic@lexmark.com',
+                 account: {name: 'Lexmark International, Inc'}, roles: 'End user'},
+                {status: 'Pending', created: '09/01/15', id: 1234567890,
+                 lastName: 'Public', firstName: 'John', email: 'jpublic@lexmark.com',
+                 account: {name: 'Lexmark International, Inc'}, roles: 'End user'},
+                {status: 'Pending', created: '09/01/15', id: 1234567890,
+                 lastName: 'Public', firstName: 'John', email: 'jpublic@lexmark.com',
+                 account: {name: 'Lexmark International, Inc'}, roles: 'End user'},
+                {status: 'Pending', created: '09/01/15', id: 1234567890,
+                 lastName: 'Public', firstName: 'John', email: 'jpublic@lexmark.com',
+                 account: {name: 'Lexmark International, Inc'}, roles: 'End user'},
+                {status: 'Pending', created: '09/01/15', id: 1234567890,
+                 lastName: 'Public', firstName: 'John', email: 'jpublic@lexmark.com',
+                 account: {name: 'Lexmark International, Inc'}, roles: 'End user'}
+            ];
+
+
+            // TODO: retrieve company name from user object
+            $scope.companyName = 'Lexmark';
+            $scope.users = $scope.all_users;
+
             $scope.columns = [{id: 1, name: 'Status'}, {id: 2, name: 'Creation date'}, {id: 3, name: 'User Id'}];
+
+            $scope.search = function() {
+                console.log('search users by text in column...');
+            };
+
+            $scope.setAllUsers = function() {
+                $scope.all_users_active = true;
+                $scope.invitations_active = false;
+                $scope.users = $scope.all_users;
+            };
+
+            $scope.setInvitations = function() {
+                $scope.all_users_active = false;
+                $scope.invitations_active = true;
+                $scope.users = $scope.invited_users;
+            };
 
         }
     ]);
