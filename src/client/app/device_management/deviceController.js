63b188d8f4e43ccfdc4b7651adfda9bb4aae2583
Rocky Bevins <moreoutput@gmail.com>
2015-09-24 13:54:02 -0400

    tests passing

--- src/client/app/device_management/deviceController.js
+++ src/client/app/device_management/deviceController.js
@@ -1,10 +1,10 @@
 define(['angular', 'deviceManagement', 'deviceManagement.deviceFactory'], function(angular) {
     'use strict';
     angular.module('mps.deviceManagement')
-    .controller('DeviceController', ['$scope', '$location', 'Device',
+    .controller('DeviceController', ['$scope', '$location', 'Devices',
         function($scope, $location, Device) {
-            $scope.goToReview = function(id) {
-                $location.path('/device_management/' + id + '/review');
+            $scope.goToReview = function(device) {
+                $location.path('/device_management/' + device.id + '/review');
             };
         }
     ]);
