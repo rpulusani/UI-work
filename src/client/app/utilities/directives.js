a63153e688e11ce237edd9144c7c01c85063bbbb
Kris Stice <kris.stice@perceptivesoftware.com>
2015-10-07 11:49:35 -0500

    Move to a directive

--- src/client/app/utilities/directives.js
+++ src/client/app/utilities/directives.js
@@ -35,5 +35,11 @@ define(['angular', 'utility'], function(angular) {
             templateUrl: '/app/utilities/templates/file-upload.html',
             controller: 'fileUploadController'
         };
+    })
+    .directive('pagination', function(){
+        return{
+            restrict: 'A',
+            templateUrl: '/app/utilities/templates/pagination.html',
+        };
     });
 });
