77a1c46ec15d210819e2286f0fa11a3111054e8b
Kim Sarenstam <ksarenstam@lexmark.com>
2015-10-07 11:39:50 +0200

    implemented service request page, and placeholder for new request.

--- src/client/app/service_requests/serviceRequest.js
+++ src/client/app/service_requests/serviceRequest.js
@@ -6,6 +6,9 @@ define(['angular'], function(angular) {
         .when('/service_requests', {
             templateUrl: '/app/service_requests/templates/service-request-dashboard.html'
         })
+        .when('/service_requests/requests/new', {
+            templateUrl: '/app/service_requests/templates/service-request-request-new.html'
+        })
         .when('/service_requests/order_request', {
             templateUrl: '/app/service_requests/templates/create-service-request.html'
         });
---
+++ src/client/app/service_requests/serviceRequestController.js
@@ -0,0 +1,13 @@
+define(['angular', 'serviceRequest', 'utility.gridService','serviceRequest.serviceRequestFactory'], function(angular) {
+    'use strict';
+    angular.module('mps.serviceRequests')
+    .controller('ServiceRequestController', ['$scope', '$location', 'gridService', 'ServiceRequestService', '$rootScope',
+        function($scope, $location, gridService, ServiceRequestService, $rootScope) {
+
+            $scope.newServiceRequest = function() {
+                $location.path('/service_requests/requests/new');
+            };
+
+        }
+    ]);
+});
