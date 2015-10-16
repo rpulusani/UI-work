bfdbd3d4316b5aadc04f6a96a30e5c59b8d0c24c
Kris Stice <kris.stice@perceptivesoftware.com>
2015-10-16 12:22:07 -0500

    Updated Devices

--- src/client/app/device_management/deviceListController.js
+++ src/client/app/device_management/deviceListController.js
@@ -2,18 +2,19 @@ define(['angular', 'deviceManagement', 'deviceManagement.deviceFactory'], functi
     'use strict';
     angular.module('mps.deviceManagement')
     .controller('DeviceListController', ['$scope', '$location', 'grid', 'Devices', '$rootScope',
-        function($scope, $location, Grid, Devices, $rootScope) {
+        'PersonalizationServiceFactory',
+        function($scope, $location, Grid, Devices, $rootScope, Personalize) {
             $rootScope.currentAccount = '1-21AYVOT';
             $rootScope.currentRowList = [];
-
+            var personal = new Personalize($location.url(),$rootScope.idpUser.id);
             $scope.goToCreate = function() {
                 $location.path('/service_requests/devices/new');
             };
 
             $scope.gridOptions = {};
-            $scope.gridOptions.onRegisterApi = Grid.getGridActions($rootScope, Devices);
+            $scope.gridOptions.onRegisterApi = Grid.getGridActions($rootScope, Devices, personal);
             Devices.getList().then(function() {
-                Grid.display(Devices, $scope);
+                Grid.display(Devices, $scope, personal);
             }, function(reason) {
                 NREUM.noticeError('Grid Load Failed for ' + Devices.serviceName +  ' reason: ' + reason);
             });
