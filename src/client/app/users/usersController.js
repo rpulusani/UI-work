f56195c99e4fcf2f35eed44e6f982aaa08f96e0d
sumandg86 <sumand@lexmark.com>
2015-10-19 14:53:33 -0400

    integration the code for passing additional parameters for invited users.

--- src/client/app/users/usersController.js
+++ src/client/app/users/usersController.js
@@ -14,7 +14,16 @@ define(['angular', 'utility.blankCheckUtility', 'user', 'user.factory'], functio
             $scope.gridOptions.onRegisterApi = Grid.getGridActions($rootScope, UserService);
 
             $scope.setGrid = function() {
-                UserService.getPage().then(function() {
+                var additionalParams = [];
+                if ($scope.invitationsActive) {
+                    additionalParams = [
+                        {
+                            name: 'type',
+                            value: 'INVITED'
+                        }
+                    ];
+                }
+                UserService.getPage(undefined,undefined,additionalParams).then(function() {
                 Grid.display(UserService, $scope, personal);
                 }, function(reason) {
                     NREUM.noticeError('Grid Load Failed for ' + UserService.serviceName +  ' reason: ' + reason);
