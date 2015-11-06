d9b86e81fe948e9ebd2c73e83de912aed6f3e202
Jay <jay.kalathia@lexmark.com>
2015-11-05 22:06:33 -0500

    report - asset register

--- src/client/app/reporting/reportController.js
+++ src/client/app/reporting/reportController.js
@@ -1,31 +1,48 @@
 define(['angular', 'report', 'chart'], function(angular) {
     'use strict';
     angular.module('mps.report')
-    .controller('ReportController', ['$scope', '$location', '$rootScope', 'Reports', 'grid',
-        function($scope, $location, $rootScope, Reports, Grid) {
+    .controller('ReportController', ['$scope', '$location', '$rootScope', 'Reports', 'grid', '$http', 'serviceUrl',
+        function($scope, $location, $rootScope, Reports, Grid, $http, serviceUrl) {
             $scope.finder = Reports.finder;
             $scope.categories = Reports.categories;
             $scope.category = Reports.category;
 
+            Reports.columns = 'fullSet';
+
             // Dummy Chart Data
+
+            // end point /devicecount
             $scope.madcEvents = [{
                 value: 300,
                 color:'#F7464A',
                 highlight: '#FF5A5E',
-                label: 'Red'
+                label: 'Shipped'
             },
             {
                 value: 50,
                 color: '#46BFBD',
                 highlight: '#5AD3D1',
-                label: 'Green'
+                label: 'Installed'
             },
             {
                 value: 100,
                 color: '#FDB45C',
                 highlight: '#FFC870',
-                label: 'Yellow'
+                label: 'Stored'
             }];
+
+            $scope.devicecount = function() {
+                $http.get(serviceUrl + "reports/devicecount/?accountId=1-1L9SRP&accountLevel=L5").success(function(res) {
+                    console.log("res", res);
+                    $scope.madcEvents[0].value = res.dataSet[0].data[0];
+                    $scope.madcEvents[1].value = res.dataSet[0].data[1];
+                    $scope.madcEvents[2].value = res.dataSet[0].data[2];
+                    console.log($scope.madcEvents);
+                });
+            };
+
+            $scope.devicecount();
+
             $scope.barChartSample = {
                 labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
                 datasets: [
@@ -61,7 +78,12 @@ define(['angular', 'report', 'chart'], function(angular) {
                 $location.path(Reports.route + '/' + Reports.category.id + '/find');
             };
 
-            $scope.runReport = function() {
+            $scope.runReport = function(category) {
+                console.log("runReport", category);
+                /*
+                  Report.get(category, '').then(function(){
+                });
+                */
                 if ($scope.finder.dateFrom && $scope.finder.dateTo) {
                     $rootScope.finder = $scope.finder;
                     $location.path(Reports.route + '/results');
