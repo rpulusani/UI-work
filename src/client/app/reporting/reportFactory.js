91e7938d53ff7b976d172390ee94bf07103594a5
Kim Sarenstam <ksarenstam@lexmark.com>
2015-12-04 10:43:42 +0100

    add storeFrontName and Chl /reporting hier/.

--- src/client/app/reporting/reportFactory.js
+++ src/client/app/reporting/reportFactory.js
@@ -12,12 +12,12 @@ define(['angular', 'report'], function(angular) {
                     defaultSet: [],
                     /* Asset Register */
                     mp9058sp: [
-                        {'name': $translate.instant('REPORTING.REPORTING_HIERARCHY'), 'field': 'chl', minWidth: 400},
-                        {'name': $translate.instant('REPORTING.ADDRESS_NAME'), 'field': 'addressName', minWidth: 250},
+                        {'name': $translate.instant('REPORTING.REPORTING_HIERARCHY'), 'field': 'chl', minWidth: 420},
+                        {'name': $translate.instant('REPORTING.ADDRESS_NAME'), 'field': 'addressName', minWidth: 240},
                         {'name': $translate.instant('REPORTING.DEVICE'), 'field': 'device', minWidth: 120},
                         {'name': $translate.instant('REPORTING.SERIAL_NUMBER'), 'field': 'serialNumber', minWidth: 150},
                         {'name': $translate.instant('REPORTING.ASSET_TAG'), 'field': 'assetTag', minWidth: 150},
-                        {'name': $translate.instant('REPORTING.DEVICE_TAG_CUSTOMER'), 'field': 'deviceTagCustomer', minWidth: 250},
+                        {'name': $translate.instant('REPORTING.DEVICE_TAG_CUSTOMER'), 'field': 'deviceTagCustomer', minWidth: 240},
                         {'name': $translate.instant('REPORTING.DEVICE_STATUS'), 'field': 'deviceStatus', minWidth: 150},
                         {'name': $translate.instant('REPORTING.LAST_LTPC'), 'field': 'lastLtpc', minWidth: 120},
                         {'name': $translate.instant('REPORTING.LAST_LTPC_DATE'), 'field': 'lastLtpcDate', 'cellFilter': 'date:\'yyyy-MM-dd\'', minWidth: 150 },
@@ -26,7 +26,7 @@ define(['angular', 'report'], function(angular) {
                         {'name': $translate.instant('REPORTING.MAC_ADDRESS'), 'field': 'macAddress', minWidth: 150},
                         {'name': $translate.instant('REPORTING.DIVISION'), 'field': 'division', minWidth: 150},
                         {'name': $translate.instant('REPORTING.LIFE_CYCLE'), 'field': 'lifeCycle', minWidth: 150},
-                        // Store_Front_Name
+                        {'name': $translate.instant('REPORTING.STORE_FRONT_NAME'), 'field': 'storeFrontName', minWidth: 210},
                         {'name': $translate.instant('REPORTING.COST_CENTER'), 'field': 'costCenter', minWidth: 150},
                         {'name': $translate.instant('REPORTING.DEPT_NUMBER'), 'field': 'departNumber', minWidth: 180},
                         {'name': $translate.instant('REPORTING.DEPT_NAME'), 'field': 'departmentName', minWidth: 180},
@@ -34,16 +34,16 @@ define(['angular', 'report'], function(angular) {
                         {'name': $translate.instant('REPORTING.TERM_START_DATE'), 'field': 'termStartDate', 'cellFilter': 'date:\'yyyy-MM-dd\'', minWidth: 150},
                         {'name': $translate.instant('REPORTING.TERM_END_DATE'), 'field': 'termEndDate', 'cellFilter': 'date:\'yyyy-MM-dd\'', minWidth: 150},
                         {'name': $translate.instant('REPORTING.TERMS'), 'field': 'terms', minWidth: 150},
-                        {'name': $translate.instant('REPORTING.ADDRESS'), 'field': 'address', minWidth: 250},
+                        {'name': $translate.instant('REPORTING.ADDRESS'), 'field': 'address', minWidth: 240},
                         {'name': $translate.instant('REPORTING.CITY'), 'field': 'city', minWidth: 150},
                         {'name': $translate.instant('REPORTING.STATE'), 'field': 'state', minWidth: 150},
                         {'name': $translate.instant('REPORTING.PROVINCE'), 'field': 'province', minWidth: 150},
                         {'name': $translate.instant('REPORTING.ZIPCODE'), 'field': 'zipcode', minWidth: 150},
                         {'name': $translate.instant('REPORTING.COUNTY'), 'field': 'county', minWidth: 150},
                         {'name': $translate.instant('REPORTING.COUNTRY'), 'field': 'country', minWidth: 150},
-                        {'name': $translate.instant('REPORTING.PHYSICAL_LOC_1'), 'field': 'phyLoc1', minWidth: 250},
-                        {'name': $translate.instant('REPORTING.PHYSICAL_LOC_2'), 'field': 'phyLoc2', minWidth: 250},
-                        {'name': $translate.instant('REPORTING.PHYSICAL_LOC_3'), 'field': 'phyLoc3', minWidth: 250},
+                        {'name': $translate.instant('REPORTING.PHYSICAL_LOC_1'), 'field': 'phyLoc1', minWidth: 240},
+                        {'name': $translate.instant('REPORTING.PHYSICAL_LOC_2'), 'field': 'phyLoc2', minWidth: 240},
+                        {'name': $translate.instant('REPORTING.PHYSICAL_LOC_3'), 'field': 'phyLoc3', minWidth: 240},
                         {'name': $translate.instant('REPORTING.AGREEMENT'), 'field': 'agreement', minWidth: 150}
                     ],
                     /* MADC */
@@ -56,12 +56,13 @@ define(['angular', 'report'], function(angular) {
                         {'name': $translate.instant('REPORTING.EVENT_TYPE'), 'field': 'type', minWidth: 150},
                         {'name': $translate.instant('REPORTING.GEO'), 'field': 'geo', minWidth: 150},
                         {'name': $translate.instant('REPORTING.MANUFACTURER'), 'field': 'manufacturer', minWidth: 150},
-                        {'name': $translate.instant('REPORTING.NEW_ADDRESS_NAME'), 'field': 'newAddress', minWidth: 250},
-                        {'name': $translate.instant('REPORTING.NEW_ASSET_SERIAL_NUMBER'), 'field': 'newSerialNumber', minWidth: 250},
+                        {'name': $translate.instant('REPORTING.NEW_ADDRESS_NAME'), 'field': 'newAddress', minWidth: 240},
+                        {'name': $translate.instant('REPORTING.NEW_ASSET_SERIAL_NUMBER'), 'field': 'newSerialNumber', minWidth: 240},
                         {'name': $translate.instant('REPORTING.NEW_IP_ADDRESS'), 'field': 'newIp', minWidth: 150},
-                        // New Rpt Hierarchy
-                        {'name': $translate.instant('REPORTING.OLD_ADDRESS_NAME'), 'field': 'oldAddress', minWidth: 150},
+                        {'name': $translate.instant('REPORTING.NEW_REPORTING_HIERARCHY'), 'field': 'newChl', minWidth: 240},
+                        {'name': $translate.instant('REPORTING.OLD_ADDRESS_NAME'), 'field': 'oldAddress', minWidth: 240},
                         {'name': $translate.instant('REPORTING.OLD_IP_ADDRESS'), 'field': 'oldIp', minWidth: 150},
+                        {'name': $translate.instant('REPORTING.OLD_REPORTING_HIERARCHY'), 'field': 'oldChl', minWidth: 240},
                         {'name': $translate.instant('REPORTING.SERIAL_NUMBER'), 'field':'origSerialNumber', minWidth: 150}
                     ],
                     /* Missing Meter Reads */
