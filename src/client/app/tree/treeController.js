846258056f5985d0d54996f0d7f2fce3f7cdef93
sumandg86 <sumand@lexmark.com>
2015-12-11 10:04:14 -0500

    Initial commit for CHL tree

--- src/client/app/tree/treeController.js
+++ src/client/app/tree/treeController.js
@@ -14,47 +14,40 @@ define([
 
             User.getLoggedInUserInfo().then(function(user) {
                 User.item._links.accounts = User.item._links.accounts[0];
-
-                // Devices.setItem(device);
-                // var options = {
-                //     params:{
-                //         embed:'contact,address'
-                //     }
-                // };
-                // Devices.item.links.self(options).then(function(){
-                //     Devices.item = Devices.item.self.item;
-                //     $location.path(Devices.route + '/' + device.id + '/review');
-
-                // });
-
                 User.getAdditional(User.item, Account).then(function() {
-                    console.log('Account', Account);
-
                     //Account.setItem(Account.item);
-                    $scope.tempItem.id = Account.item.accountId;
-                    $scope.tempItem.title = Account.item.name;
+                    console.log('amar Account ', Account.item);
+                    $scope.tempItem.accountId = Account.item.accountId;
+                    $scope.tempItem.name = Account.item.name;
+                    $scope.tempItem.level = Account.item.level;
+                    $scope.tempItem._links = {
+                        self: {}
+                    };
+                    $scope.tempItem._links.self.href = Account.item._links.self.href;
                     $scope.tempItem.items = [];
                     var options = {
                         params:{
                             embed:'childAccounts'
                         }
                     };
-                    Account.item.links.self(options).then(function(){
-                        Account.item =  Account.item.self.item;
+                    Account.item.get(options).then(function(){
+                        Account.item = Account.item.item;
                         if (Account.item._embedded.childAccounts && Account.item._embedded.childAccounts.length > 0) {
                             var childAccounts = Account.item._embedded.childAccounts;
                             for (var i=0; i<childAccounts.length; i++) {
                                 var childItem = {};
-                                childItem.id = childAccounts[i].accountId;
-                                childItem.title = childAccounts[i].name;
+                                childItem = childAccounts[i];
+                                // childItem.accountId = childAccounts[i].accountId;
+                                // childItem.name = childAccounts[i].name;
+                                // childItem.level = childAccounts[i].level;
                                 childItem._links = {
                                     self: {}
                                 };
                                 if (Account.item._links.childAccounts) {
-                                    if (Account.item._links.childAccounts.length === 1) {
+                                    if (Account.item._links.childAccounts.length) {
                                         childItem._links.self.href = Account.item._links.childAccounts[i].href;
                                     } else {
-
+                                        childItem._links.self.href = Account.item._links.childAccounts.href;
                                     }
                                 }
                                 
@@ -62,30 +55,7 @@ define([
                             }
                         }
                         $scope.items.push($scope.tempItem);
-                        console.log($scope.items);
                     });
-
-                    /*$scope.tempItem.id = Account.item.accountId;
-                    $scope.tempItem.title = Account.item.name;
-                    if (Account.item._links.childAccounts && Account.item._links.childAccounts.length > 0) {
-                        for (var i=0; i<Account.item._links.childAccounts.length; i++) {
-                            Account.getAdditional(Account.item, Addresses).then(function() {
-                                Addresses.getPage().then(function() {
-                                    Grid.display(Addresses, $scope, personal);
-                                }, function(reason) {
-                                    NREUM.noticeError('Grid Load Failed for ' + Addresses.serviceName +  ' reason: ' + reason);
-                                });
-                            });
-                        }
-                    }*/
-                    
-                    // Account.getAdditional(Account.item, Addresses).then(function() {
-                    //     Addresses.getPage().then(function() {
-                    //         Grid.display(Addresses, $scope, personal);
-                    //     }, function(reason) {
-                    //         NREUM.noticeError('Grid Load Failed for ' + Addresses.serviceName +  ' reason: ' + reason);
-                    //     });
-                    // });
                 });
             });
 
--- src/client/app/tree/treeItemController.js
+++ src/client/app/tree/treeItemController.js
@@ -2,12 +2,71 @@ define([
     'tree'
 ], function(tree){
     tree
-    .controller('TreeItemController', ['$scope',
-        function($scope){
+    .controller('TreeItemController', ['$scope', 'AccountService',
+        function($scope, Account){
             if($scope.item){
+                console.log('$scope.item',$scope.item);
+                $scope.item.items = [];
+                Account.setItem($scope.item);
+                // Account.item.params = Account.setupParams(
+                //                                 {url: Account.item._links.self.href},
+                //                                 {params:{
+                //                                     accountId: Account.item.accountId,
+                //                                     accountLevel: Account.item.level
+                //                                 }});
+                //$scope.tempItem.items = [];
+                var options = {
+                    preventDefaultParams: true,
+                    params:{
+                        accountId: Account.item.accountId,
+                        accountLevel: Account.item.level,
+                        embed:'childAccounts'
+                    }
+                };
+                Account.item.get(options).then(function(){
+                    if (Account.item && Account.item.item) {
+                        Account.item = Account.item.item;
+                    }
+                    
+                    console.log('Account.item',Account.item);
+                    if (Account.item 
+                        && Account.item._embedded 
+                        && Account.item._embedded.childAccounts 
+                        && Account.item._embedded.childAccounts.length > 0) {
+                        var childAccounts = Account.item._embedded.childAccounts;
+                        for (var i=0; i<childAccounts.length; i++) {
+                            var childItem = {};
+                            childItem = childAccounts[i];
+                            // childItem.name = childAccounts[i].name;
+                            // childItem.level = childAccounts[i].level;
+                            childItem._links = {
+                                self: {}
+                            };
+                            if (Account.item._links.childAccounts) {
+                                console.log('Account.item._links.childAccounts', Account.item._links.childAccounts);
+                                console.log('Account.item._links.childAccounts.length', Account.item._links.childAccounts.length);
+                                if (Account.item._links.childAccounts.length) {
+                                    childItem._links.self.href = Account.item._links.childAccounts[i].href;
+                                } else {
+                                    childItem._links.self.href = Account.item._links.childAccounts.href;
+                                }
+                            }
+                            
+                            $scope.item.items.push(childItem);
+                        }
+                    }
+                    console.log($scope.item);
+                });
+
+
+
                 $scope.item.disabled = false;
                 $scope.item.selected = false;
                 $scope.item.expanded = true;
+            }
+
+            $scope.toggleChildren = function() {
+
             }
 
             $scope.toggleChildren = function(item){
