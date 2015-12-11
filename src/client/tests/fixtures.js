47faab84e749e632dcfeeacf439e2c3db28b1c1f
Rocky Bevins <moreoutput@gmail.com>
2015-12-11 11:54:55 -0500

    tests for overriding acocuntId params

--- src/client/tests/fixtures.js
+++ src/client/tests/fixtures.js
@@ -102,7 +102,7 @@ define([], function() {
                 "templated" : true
             },
             "next" : {
-                "href" :  'http://127.0.0.1/test?page=1&size=20{&sort}',
+                "href" :  'http://127.0.0.1/test?page=2&size=20{&sort}',
                 "templated" : true
             }
         },
@@ -110,7 +110,7 @@ define([], function() {
             "test" : [ fixtures.api.test.itemOne, fixtures.api.test.itemTwo ]
         },
         "page" : {
-            "size" : 2,
+            "size" : 20,
             "totalElements" : 3,
             "totalPages" : 2,
             "number" : 1
