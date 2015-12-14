1f038a4c1f2968b89861d9f9a80a2f1ecfbb98f0
sumandg86 <sumand@lexmark.com>
2015-12-14 17:00:54 -0500

    Removing the display of global, legal, account or domestic levels for CHL tree

--- src/client/app/tree/treeController.js
+++ src/client/app/tree/treeController.js
@@ -7,28 +7,76 @@ define([
         function($scope, TreeItems, Account, User){
             $scope.items = [];
             $scope.selectedItems = [];
-            $scope.tempItem = {};
+            var tempItem = {};
             $scope.bulkAction = function(evt){
                 $scope.$broadcast(evt);
             };
+
+            function setChlChildren(tempItem) {
+                Account.setItem(tempItem);
+                var options = {
+                    updateParams: false,
+                    params:{
+                        accountId: Account.item.accountId,
+                        accountLevel: Account.item.level,
+                        embed:'childAccounts'
+                    }
+                };
+                Account.item.get(options).then(function(){
+                    if (Account.item && Account.item.item) {
+                        Account.item = Account.item.item;
+                    }
+
+                    if (Account.item 
+                        && Account.item._embedded 
+                        && Account.item._embedded.childAccounts 
+                        && Account.item._embedded.childAccounts.length > 0) {
+                        var childAccounts = Account.item._embedded.childAccounts;
+                        for (var i=0; i<childAccounts.length; i++) {
+                            var childItem = {};
+                            childItem = childAccounts[i];
+                            childItem._links = {
+                                self: {}
+                            };
+                            if (Account.item._links.childAccounts) {
+                                if (Account.item._links.childAccounts.length) {
+                                    childItem._links.self.href = Account.item._links.childAccounts[i].href;
+                                } else {
+                                    childItem._links.self.href = Account.item._links.childAccounts.href;
+                                }
+                            }
+                            if (childItem.level === 'global' 
+                                || childItem.level === 'legal' 
+                                || childItem.level === 'account' 
+                                || childItem.level === 'domestic') {
+                                setChlChildren(childItem);
+                            } else {
+                                $scope.items.push(childItem);
+                            }
+                        }
+                    }
+                });
+            }
+
             if ($scope.treeType && $scope.treeType === 'chl') {
                 User.getLoggedInUserInfo().then(function(user) {
                 User.item._links.accounts = User.item._links.accounts[0];
                     User.getAdditional(User.item, Account).then(function() {
-                        $scope.tempItem.accountId = Account.item.accountId;
-                        $scope.tempItem.name = Account.item.name;
-                        $scope.tempItem.level = Account.item.level;
-                        $scope.tempItem._links = {
+                        tempItem.accountId = Account.item.accountId;
+                        tempItem.name = Account.item.name;
+                        tempItem.level = Account.item.level;
+                        tempItem._links = {
                             self: {}
                         };
-                        $scope.tempItem._links.self.href = Account.item._links.self.href;
-                        $scope.tempItem.items = [];
-                        var options = {
-                            params:{
-                                embed:'childAccounts'
-                            }
-                        };
-                        $scope.items.push($scope.tempItem);
+                        tempItem._links.self.href = Account.item._links.self.href;
+                        if (tempItem.level === 'global' 
+                            || tempItem.level === 'legal' 
+                            || tempItem.level === 'account'
+                            || childItem.level === 'domestic') {
+                            setChlChildren(tempItem);
+                        } else {
+                            $scope.items.push($scope.tempItem);
+                        }
                     });
                 });
             }
