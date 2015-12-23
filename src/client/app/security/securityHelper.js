7025184fb4f634907750bf4ad2bafda7dc7ebe99
Kris Stice-Hall <kris.stice@lexmark.com>
2015-12-23 15:28:37 -0600

    Wrote Unit tests and updated code as needed.

--- src/client/app/security/securityHelper.js
+++ src/client/app/security/securityHelper.js
@@ -13,6 +13,12 @@ define(['angular', 'security'], function(angular) {
                 };
                 SecurityHelper.prototype.singlePermissionCheck = function(permissionCheckHolderName,
                     permissionToBeChecked){
+                        if(!permissionCheckHolderName){
+                            throw new Error('permissionCheckHolderName is required parameter!');
+                        }
+                        if(!permissionToBeChecked){
+                            throw new Error('permissionToBeChecked is required parameter!');
+                        }
                         scope[permissionCheckHolderName] = false;
                         scope.security.isAllowed(permissionToBeChecked).then(function(passed){
                             scope[permissionCheckHolderName]  = passed;
@@ -20,7 +26,19 @@ define(['angular', 'security'], function(angular) {
                 };
                 SecurityHelper.prototype.setupPermissionList = function(permissionCheckList){
                     var self = this;
+                    if(!permissionCheckList){
+                            throw new Error('permissionCheckHolderName is required parameter!');
+                    }
+                    if(Object.prototype.toString.call( permissionCheckList ) !== '[object Array]'){
+                        throw new Error('permissionCheckList is required to be an array!');
+                    }
                     for(var i = 0; i < permissionCheckList.length; ++i){
+                        if(permissionCheckList[i] && !permissionCheckList[i].name){
+                            throw new Error('permissionCheckList is an array but at index: ' + i + ' is missing name property');
+                        }
+                        if(permissionCheckList[i] && !permissionCheckList[i].permission){
+                            throw new Error('permissionCheckList is an array but at index: ' + i + ' is missing permission property');
+                        }
                         self.singlePermissionCheck(permissionCheckList[i].name, permissionCheckList[i].permission);
                     }
                 };
--- src/client/app/security/securityService.js
+++ src/client/app/security/securityService.js
@@ -8,43 +8,39 @@ define(['angular', 'security'], function(angular) {
             $rootScope,
             $q
             ) {
-            var workingPermissionSet,
-            SecurityService = function() {
-                workingPermissionSet = {};
-                workingPermissionSet.deferred = $q.defer();
-                var workingPermissionSetPromise = getUser();
-                 workingPermissionSetPromise.then(function(){
-                    workingPermissionSet.deferred.resolve();
-                 }, function(){
-                    workingPermissionSet.deferred.reject('Problem here');
-                });
-            };
+            var SecurityService = function() {
 
+            };
+            SecurityService.prototype.workingPermissionSet = {
+                deferred: $q.defer(),
+                data:[]
+            };
             SecurityService.prototype.isAllowed = function(requestedPermission){
                 var self = this,
                 pass = false,
                 passPromise = $q.defer();
-                if(workingPermissionSet.data && workingPermissionSet.data.length > 0){
-                    pass = checkPermission(requestedPermission);
+                if(self.workingPermissionSet.data && self.workingPermissionSet.data.length > 0){
+                    pass = self.checkPermission(requestedPermission);
                     passPromise.resolve(pass);
                 }else{
-                   workingPermissionSet.deferred.promise.then(function(){
-                        pass = checkPermission(requestedPermission);
+                    var workingPermissionSetPromise = self.getWorkingPermissionSet();
+                    workingPermissionSetPromise.then(function(){
+                        self.workingPermissionSet.deferred.resolve();
+                        pass = self.checkPermission(requestedPermission);
                         passPromise.resolve(pass);
-                    },
-                    function(reason){
+                    }, function(){
                         passPromise.resolve(pass);
-                        //output error message;
                     });
                 }
                 return passPromise.promise;
             };
 
-            function checkPermission(requestedPermission){
-                var pass = false;
-                if(workingPermissionSet && workingPermissionSet.data){
-                    for(var i = 0; i < workingPermissionSet.data.length; ++i){
-                        if(workingPermissionSet.data[i] === requestedPermission){
+            SecurityService.prototype.checkPermission = function(requestedPermission){
+                var pass = false,
+                self = this;
+                if(self.workingPermissionSet && self.workingPermissionSet.data){
+                    for(var i = 0; i < self.workingPermissionSet.data.length; ++i){
+                        if(self.workingPermissionSet.data[i] === requestedPermission){
                             console.log(requestedPermission + " Passed Check");
                             pass = true;
                             break;
@@ -52,47 +48,55 @@ define(['angular', 'security'], function(angular) {
                     }
                 }
                 return pass;
-            }
+            };
 
-            function getUser(){
-                var workingPermissionSetPromise = $q.defer();
+            SecurityService.prototype.getWorkingPermissionSet = function(){
+                var self = this,
+                workingPermissionSetPromise = $q.defer();
                 $rootScope.currentUser.deferred.promise.then(function() {
-                   workingPermissionSet.data = $rootScope.currentUser.permissions.data;
-                   if(!workingPermissionSet || workingPermissionSet.data && workingPermissionSet.data.length === 0){
-                     getPermissions($rootScope.currentUser).then(function(permissionSet){
-                        workingPermissionSet.data = permissionSet;
-                        $rootScope.currentUser.permissions.data = workingPermissionSet.data;
-                        workingPermissionSetPromise.resolve();
-                      }, function(defaultPermissionSet){
-                        workingPermissionSet.data = defaultPermissionSet;
-                        $rootScope.currentUser.permissions.data = workingPermissionSet.data;
-                        workingPermissionSetPromise.resolve();
-                      });
-                   }
+                    if(self.workingPermissionSet && (!self.workingPermissionSet.data || self.workingPermissionSet.data.length === 0)){
+                        self.getPermissions($rootScope.currentUser).then(function(permissionSet){
+                            self.workingPermissionSet.data = permissionSet;
+                            workingPermissionSetPromise.resolve(self.workingPermissionSet.data);
+                        }, function(defaultPermissionSet){
+                            self.workingPermissionSet.data = defaultPermissionSet;
+                            workingPermissionSetPromise.resolve(self.workingPermissionSet.data);
+                        });
+                    }else{
+                        workingPermissionSetPromise.resolve(self.workingPermissionSet.data);
+                    }
                 });
                 return workingPermissionSetPromise.promise;
-            }
+            };
 
-            function getPermissions(currentUser){
+            SecurityService.prototype.getPermissions  = function(currentUser){
                 var permissions = $q.defer(),
                 options = {
                     params: {}
-                };
+                },
+                defaultPermissionsSet= [];
                 options.params.accountId = currentUser.accounts[0].accountId;
                 options.params.accountLevel = currentUser.accounts[0].level;
-               currentUser.links['permissions'](options).then(function(data){
-                    if(data.permissions){
+                currentUser.links['permissions'](options).then(function(data){
+                    if(data.permissions && data.permissions.data){
+                        permissions.resolve(data.permissions.data);
+                    }else if(data.permissions && !data.permissions.data){
                         permissions.resolve(data.permissions);
                     }else{
-                        permissions.reject([]);
+                        permissions.reject(defaultPermissionsSet);
                     }
 
                 }, function (){
-                    permissions.resolve([]);
+                    permissions.resolve(defaultPermissionsSet);
                 });
 
                 return permissions.promise;
-            }
+            };
+
+            SecurityService.prototype.setWorkingPermission  = function(data){
+                var self = this;
+                self.workingPermissionSet.data = data;
+            };
 
             return SecurityService;
         }
