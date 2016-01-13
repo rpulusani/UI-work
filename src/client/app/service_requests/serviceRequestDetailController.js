ab0e563368bb6a14152384c6d2291aebcf5b12ff
Kris Stice-Hall <kris.stice@lexmark.com>
2016-01-13 16:25:09 -0600

    Adding General Details Page for all SRs

--- src/client/app/service_requests/serviceRequestDetailController.js
+++ src/client/app/service_requests/serviceRequestDetailController.js
@@ -9,6 +9,7 @@ define(['angular','serviceRequest', 'utility.grid'], function(angular) {
         'SRControllerHelperService',
         'FormatterService',
         'BlankCheck',
+        '$translate',
         function(
             $scope,
             $location,
@@ -16,8 +17,16 @@ define(['angular','serviceRequest', 'utility.grid'], function(angular) {
             ServiceRequest,
             SRHelper,
             FormatterService,
-            BlankCheck
+            BlankCheck,
+            $translate
         ) {
+
+            SRHelper.addMethods(ServiceRequest, $scope, $rootScope);
+
+            if(!ServiceRequest || !ServiceRequest.item){
+                $scope.redirectToList();
+            }
+
             $scope.sr = ServiceRequest.item;
             if(ServiceRequest && ServiceRequest.item &&
                 ServiceRequest.item.asset ){
@@ -27,55 +36,11 @@ define(['angular','serviceRequest', 'utility.grid'], function(angular) {
                 ServiceRequest.item.assetInfo){
                $scope.device =  ServiceRequest.item.assetInfo;
             }
-            switch($scope.sr.type){
-                case 'MADC_DECOMMISSION':
-                    $scope.device.lexmarkPickupDevice = true;
-                    $scope.formattedPickupDevice = FormatterService.formatYesNo($scope.device.lexmarkPickupDevice);
-                break;
-                case 'DATA_ASSET_DEREGISTER':
-                    $scope.device.lexmarkPickupDevice = false;
-                    $scope.formattedPickupDevice = FormatterService.formatYesNo($scope.device.lexmarkPickupDevice);
-                break;
-                default:
-                break;
-            }
 
-            if (!BlankCheck.isNull($scope.sr) && !BlankCheck.isNull($scope.sr.primaryContact) &&
-                !BlankCheck.isNull($scope.sr.primaryContact.item)){
-                        $scope.formattedPrimaryContact = FormatterService.formatContact($scope.sr.primaryContact.item);
-            }
-            if(!BlankCheck.isNull($scope.sr) && !BlankCheck.isNull($scope.sr.requester) &&
-                !BlankCheck.isNull($scope.sr.requester.item)){
-                $scope.requestedByContactFormatted = FormatterService.formatContact($scope.sr.requester.item);
-            }
-             if (!BlankCheck.isNull($scope.sr)) {
-                $scope.formattedNotes = FormatterService.formatNoneIfEmpty($scope.sr.notes);
-                $scope.formattedReferenceId = FormatterService.formatNoneIfEmpty($scope.sr.customerReferenceId);
-                $scope.formattedCostCenter = FormatterService.formatNoneIfEmpty($scope.sr.costCenter);
-            }
 
-            SRHelper.addMethods(ServiceRequest, $scope, $rootScope);
             $scope.configure = {
                 header: {
                     translate: {}
-                },
-                device: {
-                    information:{
-                            translate: {
-                                title: 'DEVICE_MGT.DEVICE_INFO',
-                                serialNumber: 'DEVICE_MGT.SERIAL_NO',
-                                partNumber: 'DEVICE_MGT.PART_NUMBER',
-                                product: 'DEVICE_MGT.PRODUCT_MODEL',
-                                ipAddress: 'DEVICE_MGT.IP_ADDRESS',
-                                installAddress: 'DEVICE_MGT.INSTALL_ADDRESS'
-                            }
-                    },
-                    service:{
-                        translate:{
-                            title:'DEVICE_SERVICE_REQUEST.SERVICE_DETAILS',
-                            description:'DEVICE_SERVICE_REQUEST.PROBLEM_DESCRIPTION'
-                        }
-                    }
                 },
                 contact:{
                     translate: {
@@ -107,21 +72,85 @@ define(['angular','serviceRequest', 'utility.grid'], function(angular) {
                     }
                 }
             };
-            function configureReceiptTemplate(){
-                $scope.configure.header.translate.h1 = "DEVICE_SERVICE_REQUEST.REQUEST_SERVICE_FOR_SUBMITTED";
-                $scope.configure.header.translate.body = "DEVICE_SERVICE_REQUEST.REQUEST_SERVICE_SUBMIT_HEADER_BODY";
-                $scope.configure.header.translate.bodyValues= {
-                    'srNumber': FormatterService.getFormattedSRNumber($scope.sr),
-                    'srHours': 24,
-                    'deviceManagementUrl': 'device_management/',
+            function addDeviceInformation(){
+                $scope.configure.device = {
+                    information:{
+                            translate: {
+                                title: 'DEVICE_MGT.DEVICE_INFO',
+                                serialNumber: 'DEVICE_MGT.SERIAL_NO',
+                                partNumber: 'DEVICE_MGT.PART_NUMBER',
+                                product: 'DEVICE_MGT.PRODUCT_MODEL',
+                                ipAddress: 'DEVICE_MGT.IP_ADDRESS',
+                                installAddress: 'DEVICE_MGT.INSTALL_ADDRESS'
+                            }
+                    }
+
                 };
+            }
+            function addDecommissionInfo(){
+                 $scope.configure.device.removal = {
+                            translate:{
+                                title: 'DEVICE_SERVICE_REQUEST.DEVICE_REMOVAL',
+                                pickup: 'DEVICE_SERVICE_REQUEST.DEVICE_PICKUP_LEXMARK',
+                                pageCount: 'DEVICE_SERVICE_REQUEST.DEVICE_PAGE_COUNTS'
+                            },
+                            source: 'decommission'
+                    };
+            }
+            function configureReceiptTemplate(){
+                $scope.configure.header.translate.h1 = 'SERVICE_REQUEST.REQUEST_SERVICE_FOR_SUBMITTED';
+                $scope.configure.header.translate.h1Values = {
+                    'srNumber': FormatterService.getFormattedSRNumber($scope.sr)
+                };
+                $scope.configure.header.translate.body = 'SERVICE_REQUEST.REQUEST_SERVICE_SUBMIT_HEADER_BODY';
+                $scope.configure.header.translate.bodyValues= {};
                 $scope.configure.receipt = {
                     translate:{
-                        title:"DEVICE_SERVICE_REQUEST.REQUEST_SERVICE_DETAIL",
+                        title:'SERVICE_REQUEST.REQUEST_SERVICE_DETAIL',
                         titleValues: {'srNumber': FormatterService.getFormattedSRNumber($scope.sr) }
                     }
                 };
             }
+
             $scope.setupTemplates(function(){}, configureReceiptTemplate, function(){});
+            switch($scope.sr.type){
+                case 'MADC_DECOMMISSION':
+                    addDeviceInformation();
+                    addDecommissionInfo();
+                    $scope.device.lexmarkPickupDevice = 'true';
+                    $scope.formattedPickupDevice = FormatterService.formatYesNo($scope.device.lexmarkPickupDevice);
+                break;
+                case 'DATA_ASSET_DEREGISTER':
+                    addDeviceInformation();
+                    addDecommissionInfo();
+                    $scope.device.lexmarkPickupDevice = 'false';
+                    $scope.formattedPickupDevice = FormatterService.formatYesNo($scope.device.lexmarkPickupDevice);
+                break;
+                case 'BREAK_FIX':
+                addDeviceInformation();
+                $scope.configure.device.service ={
+                        translate:{
+                            title:'DEVICE_SERVICE_REQUEST.SERVICE_DETAILS',
+                            description:'DEVICE_SERVICE_REQUEST.PROBLEM_DESCRIPTION'
+                        }
+                    };
+                break;
+                default:
+                break;
+            }
+
+            if (!BlankCheck.isNull($scope.sr) && !BlankCheck.isNull($scope.sr.primaryContact) &&
+                !BlankCheck.isNull($scope.sr.primaryContact.item)){
+                        $scope.formattedPrimaryContact = FormatterService.formatContact($scope.sr.primaryContact.item);
+            }
+            if(!BlankCheck.isNull($scope.sr) && !BlankCheck.isNull($scope.sr.requester) &&
+                !BlankCheck.isNull($scope.sr.requester.item)){
+                $scope.requestedByContactFormatted = FormatterService.formatContact($scope.sr.requester.item);
+            }
+             if (!BlankCheck.isNull($scope.sr)) {
+                $scope.formattedNotes = FormatterService.formatNoneIfEmpty($scope.sr.notes);
+                $scope.formattedReferenceId = FormatterService.formatNoneIfEmpty($scope.sr.customerReferenceId);
+                $scope.formattedCostCenter = FormatterService.formatNoneIfEmpty($scope.sr.costCenter);
+            }
     }]);
 });
