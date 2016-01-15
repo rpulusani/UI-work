e939e74bd638cc71c8a96265b0884d7ea011353e
Kim Sarenstam <ksarenstam@lexmark.com>
2016-01-15 15:01:49 +0100

    add active item.

--- src/client/app/library/library.js
+++ src/client/app/library/library.js
@@ -4,18 +4,22 @@ define(['angular'], function(angular) {
         $routeProvider
         .when('/library', {
             templateUrl: '/app/library/templates/view.html',
+            activeItem: '/library',
             controller: 'LibraryListController'
         })
         .when('/library/:id/view', {
             templateUrl: '/app/library/templates/view-document.html',
+            activeItem: '/library',
             controller: 'LibraryViewController'
         })
         .when('/library/:id/update', {
             templateUrl: '/app/library/templates/update-document.html',
+            activeItem: '/library',
             controller: 'LibraryUpdateController'
         })
         .when('/library/new', {
             templateUrl: '/app/library/templates/new-document.html',
+            activeItem: '/library',
             controller: 'LibraryNewController'
         });
     }]);
