cf596ad5502fae625eaeeb2b32520148d4fbb482
Kim Sarenstam <ksarenstam@lexmark.com>
2016-01-15 15:20:55 +0100

    remove unused code.

--- src/client/app/library/libraryNewController.js
+++ src/client/app/library/libraryNewController.js
@@ -4,28 +4,6 @@ define(['angular', 'library'], function(angular) {
     .controller('LibraryNewController', ['$scope', '$location', '$routeParams', '$translate', 'Documents', '$rootScope',
         function($scope, $location, $routeParams, $translate, Documents, $rootScope) {
 
-            function configureAddTemplates() {
-                $scope.configure = {
-                    header: {
-                        translate: {
-                            h1: 'DOCUMENT_LIBRARY.ADD_NEW_DOCUMENT.TXT_ADD_NEW_DOCUMENT',
-                            body: 'DOCUMENT_LIBRARY.ADD_NEW_DOCUMENT.TXT_ADD_NEW_DOCUMENT_PAR'
-                        }
-                    }
-                };
-            }
-
-            function configureUpdateTemplates() {
-                $scope.configure = {
-                    header: {
-                        translate: {
-                            h1: 'DOCUMENT_LIBRARY.ADD_NEW_DOCUMENT.TXT_ADD_NEW_DOCUMENT',
-                            body: 'DOCUMENT_LIBRARY.ADD_NEW_DOCUMENT.TXT_ADD_NEW_DOCUMENT_PAR'
-                        }
-                    }
-                };
-            }
-
             var redirect_to_list = function() {
                $location.path(Documents.route + '/');
             };
@@ -36,11 +14,6 @@ define(['angular', 'library'], function(angular) {
 
             if (!$routeParams.id) {
                 $scope.documentItem = {};
-                configureAddTemplates();
-
-            } else {
-                $scope.documentItem = Documents.item;
-                configureUpdateTemplates();
             }
 
         }
