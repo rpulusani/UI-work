b327e9c7ffe0787f93f05f5db3fc4be56a9b7113
Kris Stice-Hall <kris.stice@lexmark.com>
2016-01-15 11:17:38 -0600

    Updated Delegated Admin Page

--- src/client/app/users/usersFactory.js
+++ src/client/app/users/usersFactory.js
@@ -1,29 +1,31 @@
-define(['angular', 'user', 'hateoasFactory.serviceFactory'], function(angular) {
+define(['angular', 'user'], function(angular) {
     'use strict';
     angular.module('mps.user')
     .factory('UserService', [ 'serviceUrl', '$translate', 'HATEOASFactory',
         function(serviceUrl, $translate, HATEOASFactory) {
             var UserService = {
-
+                //TODO: Need to fix translations for Users
                 //customize Address
                 serviceName: 'users',
                 embeddedName: 'users',
-                columns: [
-                        {'name': $translate.instant('LABEL.STATUS'), 'field': 'activeStatus'},
-                        {'name': $translate.instant('LABEL.CREATED_DATE'), 'field':'created'},
-                        {'name': $translate.instant('LABEL.USER_ID'), 'field':'userId'},
-                        {'name': $translate.instant('LABEL.NAME_LAST_FIRST'),
-                         'cellTemplate':'<div>' +
-                                        '{{row.entity.lastName}}, {{row.entity.firstName}}' +
-                                        '</div>'
-                        },
-                        {'name': $translate.instant('LABEL.EMAIL'), 'field': 'email'},
-                        {'name': $translate.instant('LABEL.COMPANY_ACCT'), 'field': '' },
-                        {'name': $translate.instant('LABEL.ROLE'), 'field': '' }
-                ],
+                columns: 'default',
+                columnDefs: {
+                    defaultSet: [
+                            {'name': 'Status', 'field': 'activeStatus'},
+                            {'name': 'Creation date', 'field':'created'},
+                            {'name': 'User ID', 'field':'userId'},
+                            {'name': 'Name (Last, First)',
+                             'cellTemplate':'<div>' +
+                                            '{{row.entity.lastName}}, {{row.entity.firstName}}' +
+                                            '</div>'
+                            },
+                            {'name': 'Email', 'field': 'email'},
+                            {'name': 'Company account', 'field': '' },
+                            {'name': 'Roles', 'field': '' }
+                    ]
+                },
                 route: '/delegated_admin'
             };
-
             return new HATEOASFactory(UserService);
         }
     ]);
