a43631374a2515bc3820f3e67d3cb5a31fe22787
Kim Sarenstam <ksarenstam@lexmark.com>
2016-01-20 11:48:55 -0500

    and as a constant.

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -158,6 +158,7 @@ define([
     .constant('imageNowSecret', config.portal.imageNowSecret)
     .constant('imageNowUrl', config.portal.imageNowUrl)
     .constant('lbsURL', config.portal.lbsUrl)
+    .constant('libraryServiceUrl', config.portal.libraryServiceUrl)
     .constant('permissionSet', {
         dashboard:{
             view: 'VIEW_HOME_PAGE'
