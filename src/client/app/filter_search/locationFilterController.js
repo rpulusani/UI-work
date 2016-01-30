6f4d181f77ec65e21d4c4c43276881988875fc27
sumandg86 <sumand@lexmark.com>
2016-01-30 17:25:26 -0500

    removing console.logs

--- src/client/app/filter_search/locationFilterController.js
+++ src/client/app/filter_search/locationFilterController.js
@@ -4,11 +4,8 @@ define(['angular', 'filterSearch'], function(angular) {
     .controller('LocationFilterController', ['$scope', '$translate',
         function($scope, $translate) {
             $scope.locationFilter = function(selectedList){
-                console.log('selectedList', selectedList);
-                console.log('$scope.filterDef', $scope);
                 if(selectedList && $scope.filterDef && typeof $scope.filterDef === 'function'){
                     var location = selectedList.join('|');
-                    console.log('location', location);
                     $scope.params['location'] = location;
                     $scope.filterDef($scope.params, ['bookmarkFilter', 'chlFilter']);
                 }
