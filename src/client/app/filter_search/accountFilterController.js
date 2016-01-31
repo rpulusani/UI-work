3e1d12312e7758a6a319784ffceec799cbcad349
Rocky Bevins <moreoutput@gmail.com>
2016-01-31 09:43:12 -0500

    outline for account filter

---
+++ src/client/app/filter_search/accountFilterController.js
@@ -0,0 +1,9 @@
+define(['angular', 'filterSearch'], function(angular) {
+    'use strict';
+    angular.module('mps.filterSearch')
+    .controller('AccountFilterController', ['$scope', '$translate', 'OrderStatus',
+        function($scope, $translate, OrderStatus) {
+            
+        }
+    ]);
+});
