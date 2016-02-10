e6ae8997e377d64b1db03c747f48862b30c897f0
Rocky Bevins <moreoutput@gmail.com>
2016-02-10 09:02:57 -0500

    adding by way of property check

--- src/client/app/utilities/grid.js
+++ src/client/app/utilities/grid.js
@@ -203,7 +203,7 @@ define(['angular', 'utility', 'ui.grid', 'pdfmake'], function(angular) {
                 scope[self.optionsName].showBookmarkColumn = true;
                 self.serviceInfo[serviceId] = {hasBookmarkCol: true};
 
-                if (typeof scope.bookmark !== 'function' && service.item && service.item.links && service.serviceName === 'assets') {
+                if (typeof scope.bookmark !== 'function' && service.item && service.item.links && service.addBookmarkFn === true) {
                     scope.bookmark = function(rowEntity) {
                         var node = angular.element(document.getElementsByClassName('bookmark-' + rowEntity.id)[0].childNodes);
                     
