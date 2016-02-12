8f81e005683ec76fab83d377851a014fa00bb4a7
Rocky Bevins <moreoutput@gmail.com>
2016-02-12 08:41:16 -0500

    adding country to display

--- src/client/app/nav/navController.js
+++ src/client/app/nav/navController.js
@@ -14,6 +14,7 @@ define([
         'UserService',
         'AccountService',
         'HATEAOSConfig',
+        '$http',
         function(
             $scope,
             $rootScope,
@@ -22,7 +23,8 @@ define([
             Nav,
             Users,
             Accounts,
-            HATEAOSConfig
+            HATEAOSConfig,
+            $http
             ) {
 
             $scope.items = Nav.items;
