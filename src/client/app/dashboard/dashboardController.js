00a4fbd85533d2f2186feb7675551c935f144d3f
Rocky Bevins <moreoutput@gmail.com>
2016-02-15 15:47:26 -0500

    file cleanup

--- src/client/app/dashboard/dashboardController.js
+++ src/client/app/dashboard/dashboardController.js
@@ -24,154 +24,147 @@ define(['angular', 'dashboard'], function(angular) {
         Orders,
         HATEAOSConfig
     ) {
-        var personal = new Personalize($location.url(),$rootScope.idpUser.id),
-        filterSearchService = new FilterSearchService(Devices, $scope, $rootScope, personal),
-        /* The 'bar' at the top of the homepage with SR counts */
-        getSROpenCnt = function() {
-            ServiceRequests.get({
-                preventDefaultParams: true,
-                params: {
-                    accountId: $rootScope.currentAccount.accountId,
-                    accountLevel: $rootScope.currentAccount.accountLevel,
-                    status:  ['SUBMITTED', 'IN_PROCESS', 'SHIPPED']
-                }
-            }).then(function(res) {
-                $scope.srOpenCnt = ServiceRequests.page.totalElements;
-                
-                getSRCompletedCnt();
-            });
-        }, 
-        getSRCompletedCnt = function() {
-            ServiceRequests.get({
-                preventDefaultParams: true,
-                params: {
-                    accountId: $rootScope.currentAccount.accountId,
-                    accountLevel: $rootScope.currentAccount.accountLevel,
-                    status:  ['COMPLETED']
-                }
-            }).then(function(res) {
-                $scope.srCompletedCnt = {total: ServiceRequests.page.totalElements};
-            });
-        },
-        getSROrderCnt = function() {
-            Orders.get({
-                preventDefaultParams: true,
-                params: {
-                    accountId: $rootScope.currentAccount.accountId,
-                    accountLevel: $rootScope.currentAccount.accountLevel,
-                    status:  ['SUBMITTED']
-                }
-            }).then(function(res) {
-                $scope.srOrderCnt = Orders.page.totalElements;
-                getOrderCompletedCnt();
-            });
-        },
-        getOrderCompletedCnt = function() {
-            Orders.get({
-                preventDefaultParams: true,
-                params: {
-                    accountId: $rootScope.currentAccount.accountId,
-                    accountLevel: $rootScope.currentAccount.accountLevel,
-                    status:  ['COMPLETED']
-                }
-            }).then(function(res) {
-                $scope.srOrderCompletedCnt = {total: Orders.page.totalElements};
-            });
-        },
-        getSRMADCCnt = function() {
-            ServiceRequests.get({
-                preventDefaultParams: true,
-                params: {
-                    accountId: $rootScope.currentAccount.accountId,
-                    accountLevel: $rootScope.currentAccount.accountLevel,
-                    type: [
-                    /*
-                        'DATA_ASSET_CHANGE',
-                        'DATA_ASSET_DEREGISTER',
-                        'DATA_ASSET_ALL',
-                        'MADC_INSTALL',
-                        'MADC_MOVE',
-                        'MADC_DECOMMISSION',
-                        'MADC_INSTALL_AND_DECOMMISSION',
-                    */
-                        'MADC_ALL'
-                    ],
-                    status:  ['SUBMITTED']
-                }
-            }).then(function(res) {
-                //$scope.srOrderCnt = Orders.page.totalElements;
-                console.log(ServiceRequests.page.totalElements)
-                //getMADCCompletedCnt();
-            });
-        },
-        getMADCCompletedCnt = function() {
-            ServiceRequests.get({
-                preventDefaultParams: true,
-                params: {
-                    accountId: $rootScope.currentAccount.accountId,
-                    accountLevel: $rootScope.currentAccount.accountLevel,
-                    status:  ['COMPLETED']
-                }
-            }).then(function(res) {
-                $scope.srOrderCompletedCnt = {total: Orders.page.totalElements};
-            });
-        };
-
-        $scope.searchFunctionDef = function(searchVals) {
-            $location.path('/device_management');
-        };
-
         HATEAOSConfig.getCurrentAccount().then(function() {
-            getSROpenCnt();
-            getSROrderCnt();
-            getSRMADCCnt();
-        });
+            var personal = new Personalize($location.url(),$rootScope.idpUser.id),
+            filterSearchService = new FilterSearchService(Devices, $scope, $rootScope, personal),
+            /* The 'bar' at the top of the homepage with SR counts */
+            getSROpenCnt = function() {
+                ServiceRequests.get({
+                    preventDefaultParams: true,
+                    params: {
+                        accountId: $rootScope.currentAccount.accountId,
+                        accountLevel: $rootScope.currentAccount.accountLevel,
+                        status:  ['SUBMITTED', 'IN_PROCESS', 'SHIPPED']
+                    }
+                }).then(function(res) {
+                    $scope.srOpenCnt = ServiceRequests.page.totalElements;
+                    
+                    getSRCompletedCnt();
+                });
+            }, 
+            getSRCompletedCnt = function() {
+                ServiceRequests.get({
+                    preventDefaultParams: true,
+                    params: {
+                        accountId: $rootScope.currentAccount.accountId,
+                        accountLevel: $rootScope.currentAccount.accountLevel,
+                        status:  ['COMPLETED']
+                    }
+                }).then(function(res) {
+                    $scope.srCompletedCnt = {total: ServiceRequests.page.totalElements};
+                });
+            },
+            getSROrderCnt = function() {
+                Orders.get({
+                    preventDefaultParams: true,
+                    params: {
+                        accountId: $rootScope.currentAccount.accountId,
+                        accountLevel: $rootScope.currentAccount.accountLevel,
+                        status:  ['SUBMITTED']
+                    }
+                }).then(function(res) {
+                    $scope.srOrderCnt = Orders.page.totalElements;
+                    getOrderCompletedCnt();
+                });
+            },
+            getOrderCompletedCnt = function() {
+                Orders.get({
+                    preventDefaultParams: true,
+                    params: {
+                        accountId: $rootScope.currentAccount.accountId,
+                        accountLevel: $rootScope.currentAccount.accountLevel,
+                        status:  ['COMPLETED']
+                    }
+                }).then(function(res) {
+                    $scope.srOrderCompletedCnt = {total: Orders.page.totalElements};
+                });
+            },
+            getSRMADCCnt = function() {
+                ServiceRequests.get({
+                    preventDefaultParams: true,
+                    params: {
+                        accountId: $rootScope.currentAccount.accountId,
+                        accountLevel: $rootScope.currentAccount.accountLevel,
+                        type: [
+                            'MADC_ALL'
+                        ],
+                        status:  ['SUBMITTED']
+                    }
+                }).then(function(res) {
+                    $scope.srMADCCnt = ServiceRequests.page.totalElements;
 
-          $scope.hideDashboardNotification = function(){
-            $rootScope.showDashboardNotification = false;
-          };
+                    getMADCCompletedCnt();
+                });
+            },
+            getMADCCompletedCnt = function() {
+                ServiceRequests.get({
+                    preventDefaultParams: true,
+                    params: {
+                        accountId: $rootScope.currentAccount.accountId,
+                        accountLevel: $rootScope.currentAccount.accountLevel,
+                        type: [
+                            'MADC_ALL'
+                        ],
+                        status:  ['COMPLETED']
+                    }
+                }).then(function(res) {
+                    $scope.srMADCCompletedCnt = {total: ServiceRequests.page.totalElements};
+                });
+            };
+
+            $scope.searchFunctionDef = function(searchVals) {
+                $location.path('/device_management');
+            };
+
+            $scope.hideDashboardNotification = function(){
+                $rootScope.showDashboardNotification = false;
+            };
 
             // Dummy Chart Data
             $scope.columnChartObject = {};
-            $scope.columnChartObject.type = "ColumnChart";
+            $scope.columnChartObject.type = 'ColumnChart';
             $scope.columnChartObject.options = {
               'title': 'MADC Events'
             };
 
-            $scope.columnChartObject.data = {"cols": [
-                  {id: "t", label: "MADC", type: "string"},
-                  {id: "s", label: "Month", type: "number"}
-              ], "rows": [
+            $scope.columnChartObject.data = {'cols': [
+                  {id: 't', label: 'MADC', type: 'string'},
+                  {id: 's', label: 'Month', type: 'number'}
+              ], 'rows': [
                   {c: [
-                      {v: "January"},
+                      {v: 'January'},
                       {v: 65}
                   ]},
                   {c: [
-                      {v: "February"},
+                      {v: 'February'},
                       {v: 59}
                   ]},
                   {c: [
-                      {v: "March"},
+                      {v: 'March'},
                       {v: 80}
                   ]},
                   {c: [
-                      {v: "April"},
+                      {v: 'April'},
                       {v: 81}
                   ]},
                   {c: [
-                      {v: "May"},
+                      {v: 'May'},
                       {v: 56}
                   ]},
                   {c: [
-                      {v: "June"},
+                      {v: 'June'},
                       {v: 55}
                   ]},
                   {c: [
-                      {v: "July"},
+                      {v: 'July'},
                       {v: 40}
                   ]},
-              ]};
+            ]};
 
-        }
-    ]);
+            // Calls to setup action bar
+            getSROpenCnt();
+            getSROrderCnt();
+            getSRMADCCnt();
+        });
+    }]);
 });
