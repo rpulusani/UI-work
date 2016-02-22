2652e6eb0c182362d81482f5380bbb3d63b00c41
Jill Ducey <Jill.Ducey@lexmark.com>
2016-02-22 17:36:12 -0600

    Address Add Translations (queue translations still in place)

--- src/client/app/address_service_requests/addressesFactory.js
+++ src/client/app/address_service_requests/addressesFactory.js
@@ -10,7 +10,7 @@ define(['angular', 'address', 'utility.formatters', 'hateoasFactory.serviceFacto
                 columnDefs: {
                     defaultSet: [
                         {'name': 'id', 'field': 'id', 'notSearchable': true, visible:false},
-                        {'name': $translate.instant('ADDRESS.NAME'), 'field': 'name', width: "17%",
+                        {'name': $translate.instant('ADDRESS_MAN.COMMON.TXT_ADDRESS_NAME'), 'field': 'name', width: "17%",
                                 'cellTemplate':'<div>' +
                                     '<a href="#" ng-click="grid.appScope.addresses.goToUpdate(row.entity);" ' +
                                     'ng-if="grid.appScope.addressAccess">{{row.entity.name}}</a>' +
