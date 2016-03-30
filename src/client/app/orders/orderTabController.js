f68a1756dc6291532b7d9663f0427e07471150c5
sumandg86 <sumand@lexmark.com>
2016-03-30 11:29:58 -0400

    Fix for showing order tabs based on permission

--- src/client/app/orders/orderTabController.js
+++ src/client/app/orders/orderTabController.js
@@ -14,23 +14,30 @@ angular.module('mps.orders')
     ) {
         new SecurityHelper($rootScope).redirectCheck($rootScope.orderAccess);
         $scope.active = function(value){
-            $rootScope.serviceTabSelected = value;
+            $rootScope.currentOrderTab = value;
             $location.search('tab', value);
         };
 
         $scope.isActive = function(value){
             var passed = false;
-            if($rootScope.serviceTabSelected === value){
+            if($rootScope.currentOrderTab === value){
                 passed = true;
             }
             return passed;
         };
         var tabId = $location.search().tab;
         if(tabId){
-            $rootScope.currentServiceRequestTab = tabId;
+            $rootScope.currentOrderTab = tabId;
             $scope.isActive(tabId);
         }else{
-         $scope.active('orderAllTab');
+            if ($rootScope.viewHardwareOrderAccess && $rootScope.viewSupplyOrderAccess) {
+                $scope.active('orderAllTab');
+            } else if ($rootScope.viewHardwareOrderAccess && !$rootScope.viewSupplyOrderAccess) {
+                $scope.active('deviceOrderTab');
+            } else if (!$rootScope.viewHardwareOrderAccess && $rootScope.viewSupplyOrderAccess) {
+                $scope.active('supplyOrderTab');
+            }
+         
         }
     }
 ]);
