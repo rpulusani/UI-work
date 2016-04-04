19f3be27bfb50605b1d0894a324bf7a52c278d91
Rocky Bevins <moreoutput@gmail.com>
2016-04-04 10:53:55 -0400

    placeholder

--- src/client/app/utilities/grid.js
+++ src/client/app/utilities/grid.js
@@ -37,9 +37,9 @@ angular.module('mps.utility')
 
                     if (sortColumns.length == 0) {
                         currentDir = null;
-                            service.params.sort = null;
+                        service.params.sort = null;
                     } else {
-                            currentDir = sortColumns[0].sort.direction.toUpperCase();
+                        currentDir = sortColumns[0].sort.direction.toUpperCase();
 
                         if (!sortColumns[0].colDef.searchOn) {
                             service.params.sort = sortColumns[0].field;
@@ -48,18 +48,18 @@ angular.module('mps.utility')
                         }
                     }
 
-                        service.params.direction = currentDir;
+                    service.params.direction = currentDir;
 
-                        if (service.springSorting) {
-                            service.params[service.params.sort + '.dir'] = currentDir;
+                    if (service.springSorting) {
+                        service.params[service.params.sort + '.dir'] = currentDir;
                         service.params.direction = null;
-                        }
+                    }
 
                     self.gridOptions.data = [];
 
                     service.get().then(function (data) {
-                            self.gridOptions.data = self.getDataWithDataFormatters(service.data, service.functionArray);
-                            $rootScope.gridApi.core.refresh();
+                        self.gridOptions.data = self.getDataWithDataFormatters(service.data, service.functionArray);
+                        $rootScope.gridApi.core.refresh();
                     });
                 });
             }
@@ -236,10 +236,11 @@ angular.module('mps.utility')
         scope[self.optionsName].exporterAllDataFn = function() {
             scope[self.optionsName].currentGridData = scope[self.optionsName].data;
             scope[self.optionsName].servicePage = service.page;
+            //service.page.totalElements
+            return service.getPage(0, 1000).then(function() {
+                scope[self.optionsName].data = service.data;
 
-            return service.getPage(0, service.page.totalElements).then(function() {
-                 $timeout(function() {
-                    scope[self.optionsName].data = service.data;
+                $timeout(function() {
                     $timeout(function() {
                         service.page = scope[self.optionsName].servicePage;
                         scope[self.optionsName].data = scope[self.optionsName].currentGridData;
