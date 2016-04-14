8284dcf10cd9eedeb4428645c4d872d25c1262ca
Kris Stice-Hall <kris.stice@lexmark.com>
2016-04-14 14:46:08 -0500

    Removed Docker

--- codedeploy/scripts/application-stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-containerName="mps-ui"
-
-isRunning=`docker ps --filter "name=$containerName" | tail -n 1 | awk '{print $1}'`
-container="CONTAINER"
-if [[ $isRunning != $container ]]; then
-  docker stop $containerName
-  docker rm $containerName
-fi
