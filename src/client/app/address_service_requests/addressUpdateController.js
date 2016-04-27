496fb5a05a3efe58e904b4846d0f88ba07836c36
sumandg86 <sumand@lexmark.com>
2016-04-26 21:57:00 -0400

    Removing fields for address SRs

--- src/client/app/address_service_requests/addressUpdateController.js
+++ src/client/app/address_service_requests/addressUpdateController.js
@@ -378,9 +378,9 @@ angular.module('mps.serviceRequestAddresses')
                     },
                     show: {
                         referenceId: true,
-                        costCenter: true,
-                        comments: true,
-                        attachements: true
+                        costCenter: false,
+                        comments: false,
+                        attachements: false
                     }
                 },
                 actions: {
