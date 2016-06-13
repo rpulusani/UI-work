d9a68003e3d83cdedbeb5debddfcb4a43d92df60
Rocky Bevins <rockybevins@gmail.com>
2016-06-13 06:30:07 -0400

    fix for full reset

--- src/client/app/utilities/columnPickerController.js
+++ src/client/app/utilities/columnPickerController.js
@@ -3,10 +3,10 @@ Controller for the Column Picker Directive.
 Builds a list of links that modify the columns on
 a target Grid.
 */
-
+'use strict'
 angular.module('mps.utility')
-.controller('ColumnPickerController', ['$scope', '$element', '$attrs', '$translate', '$rootScope',
-    function(scope, element, attrs, translate, $rootScope) {
+.controller('ColumnPickerController', ['$scope', '$element', '$attrs', '$translate', '$rootScope', '$compile',
+    function(scope, element, attrs, translate, $rootScope, $compile) {
         var node = element[0],
         $ = require('jquery'),
         columns = [],
@@ -35,15 +35,16 @@ angular.module('mps.utility')
             return gridOptions;
         },
         createColumnSelection = function(column, gridOptions) {
-            var listItem = angular.element('<div class="col-1-3">'),
-            checkBoxWrapper = $('<div class="form__field form__field--checkbox"></div>'),
-            checkbox = $('<input type="checkbox" id="' + column.name + '" name="' + column.field  + '" value="">'),
+            var listItem = $('<div class="col-1-3">'),
+            checkboxWrapper = $('<div class="form__field form__field--checkbox"></div>'),
+            checkbox = $('<input type="checkbox" id="' + column.name.replace(/ /g, '-') + '" name="' + column.field  + '" value="">'),
             checkboxLabel = $('<label for="' + column.name + '"><span></span> ' + column.name.replace(/\s*\(.*?\)\s*/g, '') + '</label>');
 
-            checkBoxWrapper.append(checkbox);
-            checkBoxWrapper.append(checkboxLabel);
+            checkboxWrapper.append(checkbox);
+            checkboxWrapper.append(checkboxLabel);
 
             if (column.visible !== false) {
+                column.default = true;
                 checkbox.checked = true;
                 checkboxLabel.addClass('form__field--is-checked');
             } else {
@@ -52,67 +53,20 @@ angular.module('mps.utility')
 
             checkbox.customInput();
 
-            if (column.field !== 'select_all') {
-                if (checkbox && (column.dynamic === undefined || column.dynamic === true)) {
-                    checkbox.on('change', function(e) {
-                        e.preventDefault();
-
-                        if (checkbox.checked === false) {
-                            checkbox.checked = true;
-                            gridOptions = addColumn(column, gridOptions);
-                            checkboxLabel.toggleClass('form__field--is-checked');
-
-                            $rootScope.$broadcast('columnPickerSelect', column);
-                        } else {
-                            checkbox.checked = false;
-                            gridOptions = removeColumn(column, gridOptions);
-                            checkboxLabel.toggleClass('form__field--is-checked');
-                        
-                            $rootScope.$broadcast('columnPickerDeselect', column);
-                        }
-
-                        scope.$root.gridApi.core.refresh();
-                        scope.$apply();
-                    });
-
-                    listItem.append(checkBoxWrapper);
-
-                    return listItem;
-                } else {
-                    return false;
-                }
-            } else {
-                checkbox.on('change', function(e) {
-                    var i = 0;
-
+            if (checkbox) {
+                checkboxWrapper.on('click', function(e) {
                     e.preventDefault();
 
                     if (checkbox.checked === false) {
                         checkbox.checked = true;
-
-                        for (i; i < columns.length; i += 1) {
-                            console.log(i, columns.length, columns[i]);
-
-                            if (columns[i].field !== 'select_all') {
-                                gridOptions = addColumn(columns[i], gridOptions);
-                            
-                                checkboxLabel =  $('<label for="' + columns[i].name + '"><span></span> ' + columns[i].name.replace(/\s*\(.*?\)\s*/g, '') + '</label>');
-                                checkboxLabel.toggleClass('form__field--is-checked');
-                            }
-                        }
+                        gridOptions = addColumn(column, gridOptions);
+                        checkboxLabel.toggleClass('form__field--is-checked');
 
                         $rootScope.$broadcast('columnPickerSelect', column);
                     } else {
                         checkbox.checked = false;
-
-                        for (i; i < columns.length; i += 1) {
-                            if (columns[i].field !== 'select_all') {
-                                gridOptions = removeColumn(columns[i], gridOptions);
-                                
-                                checkboxLabel =  $('<label for="' + columns[i].name + '"><span></span> ' + columns[i].name.replace(/\s*\(.*?\)\s*/g, '') + '</label>');
-                                checkboxLabel.toggleClass('form__field--is-checked');
-                            }
-                        }
+                        gridOptions = removeColumn(column, gridOptions);
+                        checkboxLabel.toggleClass('form__field--is-checked');
                     
                         $rootScope.$broadcast('columnPickerDeselect', column);
                     }
@@ -121,12 +75,13 @@ angular.module('mps.utility')
                     scope.$apply();
                 });
 
-                listItem.append(checkBoxWrapper);
+                listItem.append(checkboxWrapper);
 
                 return listItem;
+            } else {
+                return false;
             }
         },
-        columns = [],
         prop;
 
         scope.$on('setupColumnPicker', function(e, Grid) {
@@ -137,15 +92,107 @@ angular.module('mps.utility')
                 list = angular.element('<div class="form"></div>'),
                 selectorContent,
                 links = [],
+                gridOptions = e.targetScope.gridOptions,
                 dropdownBtn = $(
                     '<p class="l-visible--lt-laptop"><strong>' + translate.instant('COLUMNPICKER.CUSTOMIZE_COLUMNS') + '</strong></p><div class="dropdown columnpicker" data-column-count="' + Grid.gridOptions.columnDefs.length + '">' +
                         '<button class="btn dropdown__trigger"><i class="icon icon--ui icon--navicon-secondary rotate"></i></button>' +
                     '</div>'),
-                dropdownMenu = $('<div class="row l-hidden"><div class="col-lg-3-4 col-md-1-1"><div class="row l-pad columnpicker__menu"><div class="col-1-1"><h2>' +
-                    translate.instant('COLUMNPICKER.TITLE') + '</h2></div></div></div></div>');
+                dropdownMenu = $('<div class="row l-hidden"><div class="col-lg-3-4 col-md-1-1"><div class="row l-pad columnpicker__menu">' + 
+                    '<div class="col-1-2"><h2>' + translate.instant('COLUMNPICKER.TITLE') + '</h2></div>' + 
+                    '<div class="col-1-2"><ul>' + 
+                        '<li><a href="#" ng-click="selectAll()">Select All</a></li>' + 
+                        '<li><a href="#" ng-click="deselectAll()" href="">Select None</a></li>' + 
+                        '<li><a href="#" ng-click="resetColumns()" href="">Reset Columns</a></li>' + 
+                    '</ul></div>' + 
+                    '</div></div></div>');
+
+                scope.selectAll = function() {
+                    var i = 0,
+                    checkbox,
+                    checkboxLabel,
+                    column;
+
+                    for (i; i < columns.length; i += 1) {
+                        if (columns[i].inColumnSelector) {
+                            column = columns[i];
+                            
+                            checkbox = $('#' + column.name.replace(/ /g, '-'));
+
+                            checkboxLabel = checkbox.parent().find('label');
+
+                            if (checkboxLabel.hasClass('form__field--is-checked') === false) {
+                                checkbox.checked = true;
+
+                                gridOptions = addColumn(column, gridOptions);
+
+                                checkboxLabel.toggleClass('form__field--is-checked');
+                            }
+                        }
+                    }
+                    
+                    scope.$root.gridApi.core.refresh();
+                };
+
+                scope.deselectAll = function() {
+                    var i = 0,
+                    checkbox,
+                    checkboxLabel,
+                    column;
+
+                    for (i; i < columns.length; i += 1) {
+                        if (columns[i].inColumnSelector) {
+                            column = columns[i];
+                            
+                            checkbox = $('#' + column.name.replace(/ /g, '-'));
+
+                            checkboxLabel = checkbox.parent().find('label');
+
+                            if (checkboxLabel.hasClass('form__field--is-checked') === true) {
+                                checkbox.checked = false;
+
+                                gridOptions = removeColumn(column, gridOptions);
+
+                                checkboxLabel.toggleClass('form__field--is-checked');
+                            }
+                        }
+                    }
+                    
+                    scope.$root.gridApi.core.refresh();
+                };
+
+                scope.resetColumns = function() {
+                    var i = 0,
+                    checkbox,
+                    checkboxLabel,
+                    column;
+
+                    scope.deselectAll();
+
+                    for (i; i < columns.length; i += 1) {
+                        if (columns[i].inColumnSelector && columns[i].default) {
+                            column = columns[i];
+                            
+                            checkbox = $('#' + column.name.replace(/ /g, '-'));
+
+                            checkboxLabel = checkbox.parent().find('label');
+
+                            if (checkboxLabel.hasClass('form__field--is-checked') === false) {
+                                checkbox.checked = true;
+
+                                gridOptions = addColumn(column, gridOptions);
+
+                                checkboxLabel.toggleClass('form__field--is-checked');
+                            }
+                        }
+                    }
+                    
+                    scope.$root.gridApi.core.refresh();
+                };
 
                 element.addClass('columnpicker');
                 element.append(dropdownBtn);
+
+                dropdownMenu.replaceWith($compile(dropdownMenu)(scope));
 
                 element.on('click', function(evt) {
                     var parentWrapper = dropdownBtn.parent().parent();
@@ -158,23 +205,16 @@ angular.module('mps.utility')
                         dropdownMenu.removeClass('l-hidden');
                         dropdownBtn.addClass('active');
 
-                        if (e.targetScope.gridOptions) {
-                            if (Grid.gridOptions.columnDefs.length > 0) {
-                                columns = e.targetScope.gridOptions.columnDefs;
+                        if (gridOptions) {
+                            for (i; i < Grid.gridOptions.columnDefs.length; i += 1) {
+                                if (Grid.gridOptions.columnDefs[i].field !== 'bookmark' 
+                                    && !((Grid.gridOptions.columnDefs[i].showInColumnPicker !== undefined) && (Grid.gridOptions.columnDefs[i].showInColumnPicker === false))
+                                    && (Grid.gridOptions.columnDefs[i].dynamic === undefined || Grid.gridOptions.columnDefs[i].dynamic === true)) {
+                                    Grid.gridOptions.columnDefs[i].inColumnSelector = true;
 
-                                columns.push({
-                                    field: 'select_all',
-                                    name: 'Select All',
-                                    visible: false
-                                });
-                            }
+                                    columns.push(Grid.gridOptions.columnDefs[i]);
 
-                            for (i; i < columns.length; i += 1) {
-                                var hide = ((columns[i].showInColumnPicker !== undefined) && (columns[i].showInColumnPicker === false));
-
-                                if (columns[i].field !== 'bookmark' && !hide) {
-                                    columns[i].inColumnSelector = true;
-                                    links.push(createColumnSelection(columns[i], e.targetScope.gridOptions));
+                                    links.push(createColumnSelection(Grid.gridOptions.columnDefs[i], gridOptions));
                                 }
                             }
 
