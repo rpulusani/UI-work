3a8fcfe51a09f8d85603dcdf5e45117aec4b63fc
Ujjal Bhaskar <ujjal1991@gmail.com>
2016-06-29 01:24:09 +0530

    Changes for filter by account in document library for documentlibrary

--- src/client/app/filter_search/accountAllFilterController.js
+++ src/client/app/filter_search/accountAllFilterController.js
@@ -1,9 +1,24 @@
 angular.module('mps.filterSearch')
-.controller('AccountAllFilterController', ['$scope', '$translate', 'LibraryAccounts',
-    function($scope, $translate, LibraryAccounts) {
+.controller('AccountAllFilterController', ['$scope', '$translate', 'LibraryAccounts', 'AllAccounts', 
+    function($scope, $translate, LibraryAccounts, AllAccountsService) {
         $scope.showClearMessage = false;
         $scope.selectedAccountsList = [];
         $scope.accounts = [];
+        $scope.searchAccountObj = {};
+        $scope.filteredAccountsList = [];
+
+        $scope.$watch('filteredAccountsList.length',function(newVal){
+            $scope.selectedAccountsList = [];
+            if($scope.filteredAccountsList.length > 0){
+                for(var i = 0; i < $scope.filteredAccountsList.length; i++){
+                    $scope.selectedAccountsList.push($scope.filteredAccountsList[i].accountId);
+                }
+                $scope.initiateSearch();
+            }
+            else{
+                $scope.clearAccountsFilter();
+            }
+        });
 
         LibraryAccounts.get().then(function() {
             var accountsList = LibraryAccounts.data;
@@ -15,6 +30,36 @@ angular.module('mps.filterSearch')
                 account.selected = false;
                 $scope.accounts.push(account);
             }
+            if(accountsList.length === 0){
+                $scope.searchAccounts = true;
+                $scope.searchAccountObj = {};
+            }
+        });
+
+        $scope.setAccounts = function() {
+            $scope.$broadcast('searchAccount');
+        };
+
+
+        $scope.$on('searchAccount', function(evt){
+            $scope.accountList = [];
+            if($scope.searchAccountObj.name && $scope.searchAccountObj.name.length >=3) { 
+                var options = {
+                    preventDefaultParams: true,
+                    params:{
+                        searchTerm: encodeURIComponent($scope.searchAccountObj.name)
+                    }
+                };
+                AllAccountsService.get(options).then(function(){
+                    $scope.accountList = [];
+                    if (AllAccountsService.item._embedded && AllAccountsService.item._embedded.accounts) {
+                        var allAccountList = AllAccountsService.item._embedded.accounts;
+                        for (var i=0; i<allAccountList.length; i++) {
+                            $scope.accountList.push(allAccountList[i]);
+                        }
+                    }
+                });
+            }            
         });
 
         $scope.accountsFilter = function(account) {
@@ -25,7 +70,10 @@ angular.module('mps.filterSearch')
                     $scope.selectedAccountsList.splice($scope.selectedAccountsList.indexOf(account.accountId), 1);
                 }
             }
+            $scope.initiateSearch();
+        };
 
+        $scope.initiateSearch = function(){            
             if ($scope.selectedAccountsList && $scope.filterDef && typeof $scope.filterDef === 'function'){
                 if ($scope.selectedAccountsList.length > 0) {
                     $scope.showClearMessage = true;
@@ -43,12 +91,21 @@ angular.module('mps.filterSearch')
                     $scope.filterDef($scope.params, ['accountIds']);
                 }
             }
-        };
+        }
 
         $scope.clearAccountsFilter = function() {
             if($scope.filterDef && typeof $scope.filterDef === 'function') {
                 $scope.params = {};
                 $scope.noOfAccountsSelected = 0;
+                if($scope.searchAccounts){
+                    var $ = require('jquery');
+                    $('.accountTree input[type=checkbox]').each(function(){
+                        if(this.checked){
+                            var x = this.id;
+                            $(this).trigger('click');
+                          }
+                    });
+                }
                 $scope.selectedAccountsList = [];
                 $scope.showClearMessage = false;
 
