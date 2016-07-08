0b649335a15af717275e9cd2790c025aa912bcd8
Arko Sarkar <arkosarkar343@gmail.com>
2016-07-08 14:54:07 -0400

    Changes for order Details 25836

--- src/client/app/orders/orderSupplyController.js
+++ src/client/app/orders/orderSupplyController.js
@@ -53,76 +53,78 @@ angular.module('mps.orders')
     
     if(Devices.item){
         
-        
-        if(!$rootScope.currentAccount){
-        	$rootScope.currentAccount = {};
-        }
-        $rootScope.currentAccount.accountId = Devices.item._embedded.account.accountId; 
-        $rootScope.currentAccount.accountLevel = 'siebel';
-        Agreement.params = {
-        		assetFlag : true,
-        		assetId : Devices.item.id,
-        		type : 'SUPPLIES'
-        };
-        Devices.getAdditional(Devices.item,Agreement,'agreement').then(function(){
-        	
-        	Orders.tempSpace.catalogCart.agreement = Agreement.item;
-        	$scope.supplyMaxQuantity = (Orders.tempSpace.catalogCart.agreement.supplyMaxQuantity === 0)?1:Orders.tempSpace.catalogCart.agreement.supplyMaxQuantity;
-        	$scope.serviceMaxQuantity = (Orders.tempSpace.catalogCart.agreement.serviceMaxQuantity === 0)?1:Orders.tempSpace.catalogCart.agreement.serviceMaxQuantity;
-          
-           
-            
-            Contracts.params.type =  Agreement.params.type;
-            Agreement.getAdditional(Agreement.item,Contracts,'contracts',true).then(function(){
-            	Orders.tempSpace.catalogCart.contract = Contracts.data[0];            	
-            });
-            
-            
-          
-            	AssetParts.params = {
-                		search : Devices.item.specialUsage,
-                		searchOn : 'specialUsage',
-                		supplyParam: false,
-                        serviceParam: false
-                };
-          
-            	 if(BlankCheck.checkNotBlank(Orders.tempSpace.catalogCart.agreement.supplyCatalogType)
-                		 && Orders.tempSpace.catalogCart.agreement.supplyCatalogType.toLowerCase().indexOf('supply')!== -1){
-            		 AssetParts.params.supplyParam = true;
-                 }
-                 if(BlankCheck.checkNotBlank(Orders.tempSpace.catalogCart.agreement.serviceCatalogType)
-                		 && Orders.tempSpace.catalogCart.agreement.serviceCatalogType.toLowerCase().indexOf('service')!== -1){
-                	 AssetParts.params.serviceParam = true;
-                 }
+        if(Devices.getRelationship('agreement',Devices.item) !== undefined){
+	           if(!$rootScope.currentAccount){
+	             	$rootScope.currentAccount = {};
+	           }
+        	   $rootScope.currentAccount.accountId = Devices.item._embedded.account.accountId; 
+               $rootScope.currentAccount.accountLevel = 'siebel';
+               Agreement.params = {
+               		assetFlag : true,
+               		assetId : Devices.item.id,
+               		type : 'SUPPLIES'
+               };
+               Devices.getAdditional(Devices.item,Agreement,'agreement').then(function(){
+               	
+               	Orders.tempSpace.catalogCart.agreement = Agreement.item;
+               	$scope.supplyMaxQuantity = (Orders.tempSpace.catalogCart.agreement.supplyMaxQuantity === 0)?1:Orders.tempSpace.catalogCart.agreement.supplyMaxQuantity;
+               	$scope.serviceMaxQuantity = (Orders.tempSpace.catalogCart.agreement.serviceMaxQuantity === 0)?1:Orders.tempSpace.catalogCart.agreement.serviceMaxQuantity;
                  
-            Devices.getAdditional(Devices.item, AssetParts,'parts').then(function(){
-            	AssetParts.params = {};
-                var Grid = new GridService();
-                $scope.assetParts = AssetParts.data;
-                if (AssetParts.data && AssetParts.data.length > 0) {
-                    $scope.displayOrderContent = true;                    
-                }
-                $scope.catalogOptions = {};
-                $scope.catalogOptions.onRegisterAPI = Grid.getGridActions($scope,
-                                AssetParts, personal,'catalogAPI');
-                Grid.setGridOptionsName('catalogOptions');
-                $scope.catalogOptions.showBookmarkColumn = false;
-                $scope.catalogOptions.enableRowHeaderSelection = false;
-                $scope.catalogOptions.enableFullRowSelection = false;
-                $scope.hideShowPriceColumn(AssetParts);
-                AssetParts.getThumbnails();
-                $q.all(AssetParts.thumbnails).then(function(){
-                	Grid.display(AssetParts,$scope,personal, 92, function(){
-                		setTimeout(function(){
-                			$scope.gridLoading = false;	
-                		},0);                    	
-                    });
-                    
-                });
-                $rootScope.currentAccount = undefined;
-            });
+                  
+                   
+                   Contracts.params.type =  Agreement.params.type;
+                   Agreement.getAdditional(Agreement.item,Contracts,'contracts',true).then(function(){
+                   	Orders.tempSpace.catalogCart.contract = Contracts.data[0];            	
+                   });
+                   
+                   
+                 
+                   	AssetParts.params = {
+                       		search : Devices.item.specialUsage,
+                       		searchOn : 'specialUsage',
+                       		supplyParam: false,
+                               serviceParam: false
+                       };
+                 
+                   	 if(BlankCheck.checkNotBlank(Orders.tempSpace.catalogCart.agreement.supplyCatalogType)
+                       		 && Orders.tempSpace.catalogCart.agreement.supplyCatalogType.toLowerCase().indexOf('supply')!== -1){
+                   		 AssetParts.params.supplyParam = true;
+                        }
+                        if(BlankCheck.checkNotBlank(Orders.tempSpace.catalogCart.agreement.serviceCatalogType)
+                       		 && Orders.tempSpace.catalogCart.agreement.serviceCatalogType.toLowerCase().indexOf('service')!== -1){
+                       	 AssetParts.params.serviceParam = true;
+                        }
+                        
+                   Devices.getAdditional(Devices.item, AssetParts,'parts').then(function(){
+                   	AssetParts.params = {};
+                       var Grid = new GridService();
+                       $scope.assetParts = AssetParts.data;
+                       if (AssetParts.data && AssetParts.data.length > 0) {
+                           $scope.displayOrderContent = true;                    
+                       }
+                       $scope.catalogOptions = {};
+                       $scope.catalogOptions.onRegisterAPI = Grid.getGridActions($scope,
+                                       AssetParts, personal,'catalogAPI');
+                       Grid.setGridOptionsName('catalogOptions');
+                       $scope.catalogOptions.showBookmarkColumn = false;
+                       $scope.catalogOptions.enableRowHeaderSelection = false;
+                       $scope.catalogOptions.enableFullRowSelection = false;
+                       $scope.hideShowPriceColumn(AssetParts);
+                       AssetParts.getThumbnails();
+                       $q.all(AssetParts.thumbnails).then(function(){
+                       	Grid.display(AssetParts,$scope,personal, 92, function(){
+                       		setTimeout(function(){
+                       			$scope.gridLoading = false;	
+                       		},0);                    	
+                           });
+                           
+                       });
+                       $rootScope.currentAccount = undefined;
+                   });
 
-        });
+               });
+        }
+     
         
     }
 
@@ -144,7 +146,7 @@ angular.module('mps.orders')
         var added = false;
 
         for(var i = 0; i < OrderItems.data.length; ++i){
-            if(item.itemNumber === OrderItems.data[i].itemNumber){
+            if(item.itemNumber === OrderItems.data[i].itemNumber && item.billingModel === OrderItems.data[i].billingModel){
                 added = true;
                 break;
             }
