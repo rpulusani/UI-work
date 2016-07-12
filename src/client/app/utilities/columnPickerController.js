b6ba6246223ca817200108ed304922760e436f17
Debraj Dutta <debraj.dutta@wipro.com>
2016-07-12 15:28:04 +0530

    whitespace to the right of the column selector has been removed
    defect#22384

--- src/client/app/utilities/columnPickerController.js
+++ src/client/app/utilities/columnPickerController.js
@@ -95,7 +95,7 @@ angular.module('mps.utility')
                 gridOptions = e.targetScope.gridOptions,
                 dropdownBtn = $(
                     '<p class="l-visible--lt-laptop"><strong>' + translate.instant('COLUMNPICKER.CUSTOMIZE_COLUMNS') + '</strong></p><div class="dropdown columnpicker" data-column-count="' + Grid.gridOptions.columnDefs.length + '">' +
-                        '<button class="btn dropdown__trigger"><i class="icon icon--ui icon--navicon-secondary rotate"></i></button>' +
+                        '<button class="btn marR0 dropdown__trigger"><i class="icon icon--ui icon--navicon-secondary rotate"></i></button>' +
                     '</div>'),
                 dropdownMenu = $('<div class="row l-hidden"><div class="col-lg-3-4 col-md-1-1"><div class="row l-pad columnpicker__menu">' + 
                     '<div class="col-1-2"><h2>' + translate.instant('COLUMNPICKER.TITLE') + '</h2></div>' + 
