aadf3ce58c0f6037ccbf3c9da609b6ac1dbb1716
Arko Sarkar <arkosarkar343@gmail.com>
2016-07-12 17:21:01 -0400

    Changes for defect 26312

--- src/client/app/service_requests/ShipmentController.js
+++ src/client/app/service_requests/ShipmentController.js
@@ -33,24 +33,35 @@ angular.module('mps.serviceRequests')
                        {'name': 'Part Number', 'field': 'partNumber'},
                        {'name': 'Description', 'field': 'description'},
                        {'name': 'Part Type', 'field': 'type'},
-                       {'name':'Quantity', 'field':'requestedQuantity'}                   
+                       {'name':'Quantity', 'field':'getQuantity()'}                   
                        
                    ],             
                
-             }
+             },
+             functionArray: [
+                             {
+                                 name: 'getQuantity',
+                                 functionDef: function(){
+                                	 if(this.status === 'SHIPPED' || this.status === 'DELIVERED'){
+                                		 return this.quantity;
+                                	 }else {
+                                		 return this.requestedQuantity;
+                                	 }
+                                	 
+                                 }
+                             }]
        }; 
 
    
-    	
-    	
     	$scope.shipmentGridOptions = new HATEOASFactory(Shipments);
-        $scope.shipmentGridOptions.data = ($scope.items);
+        
         var Grid = new GridService();
         var personal = new Personalize($location.url(),$rootScope.idpUser.id);
         $scope.shipmentGridOptions.showBookmarkColumn = false;
         Grid.setGridOptionsName('shipmentGridOptions');
         $scope.shipmentGridOptions.onRegisterAPI = Grid.getGridActions($scope,
         $scope.shipmentGridOptions, personal);
+        $scope.shipmentGridOptions.data = Grid.getDataWithDataFormatters($scope.items, $scope.shipmentGridOptions.functionArray);
         Grid.display($scope.shipmentGridOptions,$scope,personal, 48);
         $scope.shipmentGridOptions.enableColumnMenus = false;
         
